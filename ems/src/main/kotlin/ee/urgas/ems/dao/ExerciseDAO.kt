d12d1ad3f257ae700ef61cd4dff61dd437ca3b64
kspar <kaspar.papli@gmail.com>
2018-12-17 23:26:00 +0200

    ems create student DAO sample method

---
+++ ems/src/main/kotlin/ee/urgas/ems/dao/ExerciseDAO.kt
@@ -0,0 +1,17 @@
+package ee.urgas.ems.dao
+
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
+
+fun createStudent(email: String, givenName: String, familyName: String) {
+    // Later get preferences as well
+    transaction {
+        Student.insert {
+            it[Student.email] = email
+            it[Student.givenName] = givenName
+            it[Student.familyName] = familyName
+            it[createdAt] = DateTime.now()
+        }
+    }
+}
