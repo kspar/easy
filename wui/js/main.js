3ba9b5a3a9d9781d3dd9e34382848193ba76731e
Reimo Luik <cygnus888@gmail.com>
2018-12-18 10:36:35 +0200

    Changes to templates. Started using MaterializeCSS.

--- wui/js/main.js
+++ wui/js/main.js
@@ -1,2 +1,28 @@
+$(document).ready(function() {
 
+    $("#tabs").tabs();
 
+    var answer_editor = CodeMirror.fromTextArea(answerform, {
+		mode:  "javascript",
+        lineNumbers: true,
+        autoRefresh: true
+    });
+
+    $('.collapsible').collapsible({
+        accordion: false
+    });
+
+    var answers = document.getElementsByClassName("answer-item");
+    for (var i = 0; i < answers.length; i++) {
+        editor(answers[i]);
+    }
+});
+
+function editor(id) {
+    CodeMirror.fromTextArea(id, {
+        mode: "javascript",
+        lineNumbers: true,
+        readOnly: true,
+        autoRefresh: true
+    });
+}
