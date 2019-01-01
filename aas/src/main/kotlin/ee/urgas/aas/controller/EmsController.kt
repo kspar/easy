50f5bae154f3df4ace8e730a9ff3e713bb03335e
kspar <kaspar.papli@gmail.com>
2019-01-01 20:16:05 +0200

    aas controller skeletons

---
+++ aas/src/main/kotlin/ee/urgas/aas/controller/EmsController.kt
@@ -0,0 +1,9 @@
+package ee.urgas.aas.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1")
+class EmsController {
+}
