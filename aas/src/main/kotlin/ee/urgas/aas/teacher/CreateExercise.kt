5c9e2229786a54f4470975084583e54b6fcecbf2
kspar <kaspar.papli@gmail.com>
2019-01-02 14:05:37 +0200

    aas create exercise service

---
+++ aas/src/main/kotlin/ee/urgas/aas/teacher/CreateExercise.kt
@@ -0,0 +1,99 @@
+package ee.urgas.aas.teacher
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import ee.urgas.aas.dao.Asset
+import ee.urgas.aas.dao.Executor
+import ee.urgas.aas.dao.Exercise
+import ee.urgas.aas.dao.ExerciseExecutor
+import ee.urgas.aas.exception.InvalidRequestException
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1")
+class CreateExerciseController {
+
+    data class CreateExBody(@JsonProperty("grading_script") val gradingScript: String,
+                            @JsonProperty("container_image") val containerImage: String,
+                            @JsonProperty("max_time_sec") val maxTime: Int,
+                            @JsonProperty("max_mem_mb") val maxMem: Int,
+                            @JsonProperty("assets") val assets: Set<CreateExAsset>?,
+                            @JsonProperty("executors") val executors: Set<String>)
+
+    data class CreateExAsset(@JsonProperty("file_name") val fileName: String,
+                             @JsonProperty("file_content") val fileContent: String)
+
+    data class CreatedExResponse(@JsonProperty("id") val id: String)
+
+    @PostMapping("/exercises")
+    fun createEx(@RequestBody body: CreateExBody): CreatedExResponse {
+        println(body)
+        validateCreateExBody(body)
+
+        // TODO: caller email from auth
+        val newExercise = mapToNewExercise(body, "TODO")
+
+        val exerciseId = insertExercise(newExercise)
+
+        return CreatedExResponse(exerciseId.toString())
+    }
+
+    fun validateCreateExBody(body: CreateExBody) {
+        if (body.executors.isEmpty()) {
+            throw InvalidRequestException("Must have at least one executor")
+        }
+    }
+
+    fun mapToNewExercise(dto: CreateExBody, callerEmail: String): NewExercise {
+        val assets = dto.assets?.map { NewAsset(it.fileName, it.fileContent) }?.toSet()
+        val executors = dto.executors.map { it.toLong() }.toSet()
+        return NewExercise(callerEmail, dto.gradingScript, dto.containerImage,
+                dto.maxTime, dto.maxMem, assets, executors)
+    }
+}
+
+
+data class NewExercise(val ownerEmail: String, val gradingScript: String, val containerImage: String,
+                       val maxTime: Int, val maxMem: Int, val assets: Set<NewAsset>?, val executorIds: Set<Long>)
+
+data class NewAsset(val fileName: String, val fileContent: String)
+
+fun insertExercise(newExercise: NewExercise): Long {
+    return transaction {
+        val exId = Exercise.insertAndGetId {
+            it[ownerEmail] = newExercise.ownerEmail
+            it[gradingScript] = newExercise.gradingScript
+            it[containerImage] = newExercise.containerImage
+            it[maxTime] = newExercise.maxTime
+            it[maxMem] = newExercise.maxMem
+        }
+
+        newExercise.assets?.forEach { asset ->
+            Asset.insert {
+                it[exercise] = exId
+                it[fileName] = asset.fileName
+                it[fileContent] = asset.fileContent
+            }
+        }
+
+        newExercise.executorIds.forEach { execId ->
+            if (Executor.select { Executor.id eq execId }.count() == 0) {
+                throw InvalidRequestException("Executor with id $execId does not exist")
+            }
+
+            ExerciseExecutor.insert {
+                it[exercise] = exId
+                it[executor] = EntityID(execId, Executor)
+            }
+        }
+
+        exId
+    }.value
+}
