b94d6db4c1eb22299aa585f3fd515ac153f43e10
kspar <kaspar.papli@gmail.com>
2019-01-02 15:26:30 +0200

    aas exposed explicit table names

--- aas/src/main/kotlin/ee/urgas/aas/db/Tables.kt
+++ aas/src/main/kotlin/ee/urgas/aas/db/Tables.kt
@@ -3,7 +3,7 @@ package ee.urgas.aas.db
 import org.jetbrains.exposed.dao.LongIdTable
 
 
-object Exercise : LongIdTable() {
+object Exercise : LongIdTable("exercise") {
     val ownerEmail = text("owner_email")
     val gradingScript = text("grading_script")
     val containerImage = text("container_image")
@@ -11,20 +11,20 @@ object Exercise : LongIdTable() {
     val maxMem = integer("max_mem_mb")
 }
 
-object Asset : LongIdTable() {
+object Asset : LongIdTable("asset") {
     val exercise = reference("exercise_id", Exercise)
     val fileName = text("file_name")
     val fileContent = text("file_content")
 }
 
-object Executor : LongIdTable() {
+object Executor : LongIdTable("executor") {
     val name = text("name")
     val baseUrl = text("base_url")
     val load = integer("load")
     val maxLoad = integer("max_load")
 }
 
-object ExerciseExecutor : LongIdTable() {
+object ExerciseExecutor : LongIdTable("exercise_executor") {
     val exercise = reference("exercise_id", Exercise)
     val executor = reference("executor_id", Executor)
 }
