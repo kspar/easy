fbad60d5a13be0571495df9508bf4c5c82106a28
kspar <kaspar.papli@gmail.com>
2019-01-02 14:54:37 +0200

    aas create executor

---
+++ aas/src/main/kotlin/ee/urgas/aas/admin/CreateExecutor.kt
@@ -0,0 +1,43 @@
+package ee.urgas.aas.admin
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import ee.urgas.aas.dao.Executor
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1")
+class CreateExecutorController {
+
+    data class CreateExecutorBody(@JsonProperty("name") val name: String,
+                                  @JsonProperty("base_url") val baseUrl: String,
+                                  @JsonProperty("max_load") val maxLoad: Int)
+
+    data class CreateExecutorResponse(@JsonProperty("id") val id: String)
+
+    @PostMapping("/executors")
+    fun createExecutor(@RequestBody body: CreateExecutorBody): CreateExecutorResponse {
+        val newExecutor = mapToNewExecutor(body)
+        val executorId = insertExecutor(newExecutor)
+        return CreateExecutorResponse(executorId.toString())
+    }
+
+    private fun mapToNewExecutor(dto: CreateExecutorBody): NewExecutor = NewExecutor(dto.name, dto.baseUrl, dto.maxLoad)
+}
+
+private data class NewExecutor(val name: String, val baseUrl: String, val maxLoad: Int)
+
+private fun insertExecutor(newExecutor: NewExecutor): Long {
+    return transaction {
+        Executor.insertAndGetId {
+            it[name] = newExecutor.name
+            it[baseUrl] = newExecutor.baseUrl
+            it[maxLoad] = newExecutor.maxLoad
+            it[load] = 0
+        }
+    }.value
+}
