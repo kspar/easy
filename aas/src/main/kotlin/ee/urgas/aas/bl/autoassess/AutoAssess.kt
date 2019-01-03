37dedc241687251d2173812fe2235e929ca084a9
kspar <kaspar.papli@gmail.com>
2019-01-03 16:53:40 +0200

    aas auto assessment calls to executors

--- aas/src/main/kotlin/ee/urgas/aas/bl/autoassess/AutoAssess.kt
+++ aas/src/main/kotlin/ee/urgas/aas/bl/autoassess/AutoAssess.kt
@@ -1,22 +1,31 @@
 package ee.urgas.aas.bl.autoassess
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import ee.urgas.aas.db.Asset
 import ee.urgas.aas.db.Executor
+import ee.urgas.aas.db.Exercise
 import ee.urgas.aas.db.ExerciseExecutor
 import ee.urgas.aas.exception.ForbiddenException
 import mu.KotlinLogging
+import org.jetbrains.exposed.sql.SqlExpressionBuilder
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
+
+const val EXECUTOR_GRADE_URL = "/v1/grade"
+
 private val log = KotlinLogging.logger {}
+
 
 @RestController
 @RequestMapping("/v1")
@@ -83,16 +92,25 @@ class AutoAssessController {
             AutoAssessResponse(assessment.grade, assessment.feedback)
 }
 
+
 data class AutoAssessSubmission(val exerciseId: Long, val submission: String)
+
+data class AutoAssessExercise(val gradingScript: String, val containerImage: String, val maxTime: Int, val maxMem: Int,
+                              val assets: List<AutoAssessExerciseAsset>)
+
+data class AutoAssessExerciseAsset(val fileName: String, val fileContent: String)
 
 data class AutoAssessment(val grade: Int, val feedback: String)
 
-data class CapableExecutor(val id: Long, val baseUrl: String, val load: Int, val maxLoad: Int)
+data class CapableExecutor(val id: Long, val name: String, val baseUrl: String, val load: Int, val maxLoad: Int)
+
 
 private fun assess(submission: AutoAssessSubmission): AutoAssessment {
+    val exercise = getExerciseDetails(submission.exerciseId)
+    val request = mapToExecutorRequest(exercise, submission)
     val executors = getCapableExecutors(submission.exerciseId)
     val selectedExecutor = selectExecutor(executors)
-    return callExecutor(selectedExecutor, submission)
+    return callExecutor(selectedExecutor, request)
 }
 
 private fun selectExecutor(executors: Set<CapableExecutor>): CapableExecutor {
@@ -101,21 +119,86 @@ private fun selectExecutor(executors: Set<CapableExecutor>): CapableExecutor {
     }
 }
 
-fun callExecutor(executor: CapableExecutor, submission: AutoAssessSubmission): AutoAssessment {
-    // TODO
-    println(executor)
-    return AutoAssessment(42, "forty-two")
-}
 
-private fun incExecutorLoad(executor: CapableExecutor) {
-    transaction {
-        // TODO
+data class ExecutorRequest(@JsonProperty("submission") val submission: String,
+                           @JsonProperty("grading_script") val gradingScript: String,
+                           @JsonProperty("assets") val assets: List<ExecutorRequestAsset>,
+                           @JsonProperty("image_name") val imageName: String,
+                           @JsonProperty("max_time_sec") val maxTime: Int,
+                           @JsonProperty("max_mem_mb") val maxMem: Int)
+
+data class ExecutorRequestAsset(@JsonProperty("file_name") val fileName: String,
+                                @JsonProperty("file_content") val fileContent: String)
+
+data class ExecutorResponse(@JsonProperty("grade") val grade: Int,
+                            @JsonProperty("feedback") val feedback: String)
+
+
+private fun mapToExecutorRequest(exercise: AutoAssessExercise, submission: AutoAssessSubmission): ExecutorRequest =
+        ExecutorRequest(
+                submission.submission,
+                exercise.gradingScript,
+                exercise.assets.map { ExecutorRequestAsset(it.fileName, it.fileContent) },
+                exercise.containerImage,
+                exercise.maxTime,
+                exercise.maxMem
+        )
+
+
+private fun callExecutor(executor: CapableExecutor, request: ExecutorRequest): AutoAssessment {
+    incExecutorLoad(executor.id)
+    try {
+        log.info { "Calling executor ${executor.name}, load is now ${getExecutorLoad(executor.id)}" }
+
+        val template = RestTemplate()
+        val responseEntity = template.postForEntity(
+                executor.baseUrl + EXECUTOR_GRADE_URL, request, ExecutorResponse::class.java)
+
+        if (responseEntity.statusCode.isError) {
+            log.error { "Executor error ${responseEntity.statusCodeValue} with request $request" }
+            throw RuntimeException("Executor error (${responseEntity.statusCodeValue})")
+        }
+
+        val response = responseEntity.body
+        if (response == null) {
+            log.error { "Executor response is empty with request $request" }
+            throw RuntimeException("Executor error (empty body)")
+        }
+
+        return AutoAssessment(response.grade, response.feedback)
+
+    } finally {
+        decExecutorLoad(executor.id)
+        log.info { "Call finished to executor ${executor.name}, load is now ${getExecutorLoad(executor.id)}" }
     }
 }
 
-private fun decExecutorLoad(executor: CapableExecutor) {
+
+private fun incExecutorLoad(executorId: Long) {
     transaction {
-        // TODO
+        Executor.update({ Executor.id eq executorId }) {
+            with(SqlExpressionBuilder) {
+                it.update(Executor.load, Executor.load + 1)
+            }
+        }
+    }
+}
+
+private fun decExecutorLoad(executorId: Long) {
+    transaction {
+        Executor.update({ Executor.id eq executorId }) {
+            with(SqlExpressionBuilder) {
+                it.update(Executor.load, Executor.load - 1)
+            }
+        }
+    }
+}
+
+private fun getExecutorLoad(executorId: Long): Int {
+    return transaction {
+        Executor.slice(Executor.load)
+                .select { Executor.id eq executorId }
+                .map { it[Executor.load] }[0]
     }
 }
 
@@ -123,7 +206,31 @@ private fun getCapableExecutors(exerciseId: Long): Set<CapableExecutor> {
     return transaction {
         (Executor innerJoin ExerciseExecutor)
                 .select { ExerciseExecutor.exercise eq exerciseId }
-                .map { CapableExecutor(it[Executor.id].value, it[Executor.baseUrl], it[Executor.load], it[Executor.maxLoad]) }
+                .map {
+                    CapableExecutor(
+                            it[Executor.id].value,
+                            it[Executor.name],
+                            it[Executor.baseUrl],
+                            it[Executor.load],
+                            it[Executor.maxLoad])
+                }
                 .toSet()
+    }
+}
+
+private fun getExerciseDetails(exerciseId: Long): AutoAssessExercise {
+    return transaction {
+        val assets = Asset.select { Asset.exercise eq exerciseId }
+                .map { AutoAssessExerciseAsset(it[Asset.fileName], it[Asset.fileContent]) }
+
+        Exercise.select { Exercise.id eq exerciseId }
+                .map {
+                    AutoAssessExercise(
+                            it[Exercise.gradingScript],
+                            it[Exercise.containerImage],
+                            it[Exercise.maxTime],
+                            it[Exercise.maxMem],
+                            assets)
+                }[0]
     }
 }
