05230a3a6123319bbfd34b0cb95e0c4f256fdc3b
kspar <kaspar.papli@gmail.com>
2019-01-04 17:11:13 +0200

    ems add students to course service

---
+++ ems/src/main/kotlin/ee/urgas/ems/bl/course/AddStudentsToCourse.kt
@@ -0,0 +1,60 @@
+package ee.urgas.ems.bl.course
+
+import ee.urgas.ems.db.Course
+import ee.urgas.ems.db.Student
+import ee.urgas.ems.db.StudentCourseAccess
+import ee.urgas.ems.exception.InvalidRequestException
+import mu.KotlinLogging
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.batchInsert
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+@RequestMapping("/v1")
+class AddStudentsToCourseController {
+
+    @PostMapping("/teacher/courses/{courseId}/students")
+    fun addStudentsToCourse(@PathVariable("courseId") courseId: String,
+                            @RequestBody studentEmails: List<String>) {
+
+        log.debug { "Adding access to course $courseId to students $studentEmails" }
+        try {
+            insertStudentCourseAccesses(courseId.toLong(), studentEmails)
+        } catch (e: StudentNotFoundException) {
+            log.info { "Adding failed, student not found: ${e.message}" }
+            throw InvalidRequestException(e.message)
+        }
+    }
+
+}
+
+class StudentNotFoundException(override val message: String) : RuntimeException(message)
+
+private fun insertStudentCourseAccesses(courseId: Long, emails: List<String>) {
+    transaction {
+        addLogger(StdOutSqlLogger)
+
+        emails.forEach {
+            val studentExists = Student.select { Student.id eq it }
+                    .count() == 1
+            if (!studentExists) {
+                throw StudentNotFoundException(it)
+            }
+        }
+
+        StudentCourseAccess.batchInsert(emails) { email ->
+            this[StudentCourseAccess.student] = EntityID(email, Student)
+            this[StudentCourseAccess.course] = EntityID(courseId, Course)
+        }
+    }
+}
