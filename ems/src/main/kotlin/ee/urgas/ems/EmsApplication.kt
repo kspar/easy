810fb88de368cb412ad2a35c00ea41a5efdb9711
kspar <kaspar.papli@gmail.com>
2019-01-07 00:01:19 +0200

    ems refactor db/ds conf

--- ems/src/main/kotlin/ee/urgas/ems/EmsApplication.kt
+++ ems/src/main/kotlin/ee/urgas/ems/EmsApplication.kt
@@ -1,37 +1,13 @@
 package ee.urgas.ems
 
-import org.jetbrains.exposed.sql.Database
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableAsync
-import org.springframework.stereotype.Component
-import javax.annotation.PostConstruct
-import javax.sql.DataSource
 
 @EnableAsync
 @SpringBootApplication
-class EmsApplication {
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    fun dataSource(): DataSource {
-        return DataSourceBuilder.create().build()
-    }
-}
-
-@Component
-class InitComponent(val dataSource: DataSource) {
-    @PostConstruct
-    fun init() {
-        Database.connect(dataSource)
-    }
-}
-
+class EmsApplication
 
 fun main(args: Array<String>) {
     runApplication<EmsApplication>(*args)
 }
-
