825c62686ac4b2fc5d114ace9449c5c4a903fc7a
kspar <kaspar.papli@gmail.com>
2019-01-08 13:03:45 +0200

    wui common authentication, page init & token parsing

--- wui/js/main.js
+++ wui/js/main.js
@@ -1,3 +1,131 @@
+
+/** Page-specific functions **/
+
+function editor(id) {
+    CodeMirror.fromTextArea(id, {
+        mode: "javascript",
+        lineNumbers: true,
+        readOnly: true,
+        autoRefresh: true
+    });
+}
+
+
+/** Init page functions **/
+
+function initExercisePageNoAuth() {
+    // Init tabs
+    $("#tabs").tabs();
+
+    var answer_editor = CodeMirror.fromTextArea(answerform, {
+        mode: "javascript",
+        lineNumbers: true,
+        autoRefresh: true
+    });
+
+    $('.collapsible').collapsible({
+        accordion: false
+    });
+
+    var answers = document.getElementsByClassName("answer-item");
+    for (var i = 0; i < answers.length; i++) {
+        editor(answers[i]);
+    }
+}
+
+function initCoursesPageNoAuth() {
+    console.debug("Courses page")
+
+}
+
+function initCoursesPageAuth() {
+    // TODO: fetch courses
+}
+
+function initCommonNoAuth() {
+    // Init profile dropdown menu
+    $(".dropdown-trigger").dropdown();
+
+    // Init logout link to redirect back to current page
+    const redirectUri = window.location.href;
+    // TODO: can get from kc?
+    const logoutLink = "https://idp.lahendus.ut.ee/auth/realms/master/protocol/openid-connect/logout?redirect_uri=" + encodeURIComponent(redirectUri);
+    $("#logout-link").attr("href", logoutLink);
+}
+
+function initCommonAuth() {
+    const token = kc.tokenParsed;
+    //console.log(token);
+
+    // Set display name
+    let displayName = token.given_name;
+
+    if (displayName === undefined) {
+        error("Given name undefined", token);
+        displayName = "Kasutaja";
+    }
+    $("#profile-name").text(displayName);
+
+    // Set roles
+    roles = token.easy_role;
+}
+
+
+/** General functions **/
+
+/**
+ * Initialize elements that require authentication.
+ */
+function initPageAuth() {
+    initCommonAuth();
+
+    const pageId = $("body").data("pageid");
+    console.debug("Page id: " + pageId);
+
+    switch (pageId) {
+        case "courses":
+            initCoursesPageAuth();
+            break;
+        case "exercise":
+
+            break;
+    }
+}
+
+/**
+ * Initialize elements that do not require authentication.
+ */
+function initPageNoAuth() {
+    initCommonNoAuth();
+
+    const pageId = $("body").data("pageid");
+    console.debug("Page id: " + pageId);
+
+    switch (pageId) {
+        case "courses":
+            initCoursesPageNoAuth();
+            break;
+        case "exercise":
+            initExercisePageNoAuth();
+            break;
+    }
+}
+
+function isStudent() {
+    return hasRole("student");
+}
+
+function isTeacher() {
+    return hasRole("teacher");
+}
+
+function hasRole(role) {
+    if (roles === undefined) {
+        error("Roles is undefined", new Error().stack);
+    }
+    return $.inArray(role, roles) !== -1
+}
+
 function reportError(o1, o2) {
     // TODO: report error
 }
@@ -20,42 +148,6 @@ function ensureTokenValid() {
     }
 }
 
-
-function editor(id) {
-    CodeMirror.fromTextArea(id, {
-        mode: "javascript",
-        lineNumbers: true,
-        readOnly: true,
-        autoRefresh: true
-    });
-}
-
-function exercisePage() {
-    // Init tabs
-    $("#tabs").tabs();
-
-    var answer_editor = CodeMirror.fromTextArea(answerform, {
-        mode: "javascript",
-        lineNumbers: true,
-        autoRefresh: true
-    });
-
-    $('.collapsible').collapsible({
-        accordion: false
-    });
-
-    var answers = document.getElementsByClassName("answer-item");
-    for (var i = 0; i < answers.length; i++) {
-        editor(answers[i]);
-    }
-}
-
-
-function coursesPage() {
-    console.debug("Courses page")
-
-}
-
 function authenticate() {
     kc = Keycloak();
     kc.init({
@@ -63,58 +155,37 @@ function authenticate() {
 
     }).success((authenticated) => {
         console.debug("Authenticated: " + authenticated);
+        initPageAuth();
 
     }).error(function (e) {
         error("Keycloak init failed", e);
-
+        // TODO: show error message
     });
 
     kc.onTokenExpired = () => {
         kc.updateToken();
-    }
-}
+    };
 
-function common() {
-    if (AUTH_ENABLED) {
-        authenticate();
-    }
-    // authenticate
-
-    // parse auth token to retrieve user info
-
-    // Init profile dropdown menu
-    $(".dropdown-trigger").dropdown();
-
-    // Init logout link to redirect back to current page
-    const redirectUri = window.location.href;
-    const logoutLink = "https://idp.lahendus.ut.ee/auth/realms/master/protocol/openid-connect/logout?redirect_uri=" + encodeURIComponent(redirectUri);
-    $("#logout-link").attr("href", logoutLink);
+    kc.onAuthRefreshSuccess = () => {
+        initCommonAuth(); // TODO: initPageAuth() if we want to refresh page content with token refresh
+    };
 }
 
 
-function runPageCode() {
-    const pageId = $("body").data("pageid");
-    console.debug("Page id: " + pageId);
+/** Main **/
 
-    common();
-
-    if (pageId === "courses") {
-        coursesPage()
-
-    } else if (pageId === "exercise") {
-        exercisePage()
-    }
-}
-
-
-const AUTH_ENABLED = false;
-
+const AUTH_ENABLED = true;
 const TOKEN_MIN_VALID_SEC = 20;
 
 // Keycloak object
 let kc;
+// Roles
+let roles;
 
 $(document).ready(() => {
-    runPageCode();
+    if (AUTH_ENABLED) {
+        authenticate();
+    }
+    initPageNoAuth();
 });
 
