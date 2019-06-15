ebec358804c7c2fda4dad2672b2ccb448f42e1e6
kspar <kaspar.papli@gmail.com>
2019-06-15 14:09:26 +0300

    wui Page documentation

--- wui/src/main/kotlin/pages/Page.kt
+++ wui/src/main/kotlin/pages/Page.kt
@@ -1,12 +1,26 @@
 package pages
 
 import PageId
+import PageState
 
+/**
+ * Represents a page with a unique path scheme and rendering logic.
+ * Implementations can utilize global page state in [PageState].
+ */
 interface Page {
 
+    /**
+     * Unique page id
+     */
     val pageId: PageId
 
+    /**
+     * Determine whether the given path should be served by this page.
+     */
     fun pathMatches(path: String): Boolean
 
+    /**
+     * Build the current page: fetch resources, perform requests, render templates, add listeners etc.
+     */
     fun build()
 }
