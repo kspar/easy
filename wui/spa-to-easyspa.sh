305b4e670c11f0a54e3a7c756213c37723035206
kspar <kaspar.papli@gmail.com>
2019-06-15 17:56:51 +0300

    wui deploy script copy css & js files as well

--- wui/spa-to-easyspa.sh
+++ wui/spa-to-easyspa.sh
@@ -6,6 +6,8 @@ s="s+<div id=\"date\">.*</div>+<div id=\"date\">$d</div>+g"
 sed -i "$s" spaproto.html
 
 scp spaproto.html easyspa:/var/www/html/index.html
+scp -r css/ easyspa:/var/www/html
+scp build/kotlin-js-min/main/* easyspa:/var/www/html/js/
 
 b="s+<div id=\"date\">.*</div>+<div id=\"date\"></div>+g"
 sed -i "$b" spaproto.html
