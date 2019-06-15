679f93dee7fd47c8e590f58d739c65e3b03a911b
kspar <kaspar.papli@gmail.com>
2019-06-15 18:11:53 +0300

    wui deploy script copy only changing js file

--- wui/spa-to-easyspa.sh
+++ wui/spa-to-easyspa.sh
@@ -6,8 +6,10 @@ s="s+<div id=\"date\">.*</div>+<div id=\"date\">$d</div>+g"
 sed -i "$s" spaproto.html
 
 scp spaproto.html easyspa:/var/www/html/index.html
-scp -r css/ easyspa:/var/www/html
-scp build/kotlin-js-min/main/* easyspa:/var/www/html/js/
+scp build/kotlin-js-min/main/wui.js easyspa:/var/www/html/js/
+
+#scp build/kotlin-js-min/main/* easyspa:/var/www/html/js/
+#scp -r css/ easyspa:/var/www/html
 
 b="s+<div id=\"date\">.*</div>+<div id=\"date\"></div>+g"
 sed -i "$b" spaproto.html
