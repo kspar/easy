1e3e153220503f9babc064ae111eef8274ba8951
Kaspar Papli <paplka@f-secure.com>
2019-06-18 17:27:44 +0300

    wui refactor native js object hacks

--- wui/src/main/kotlin/templating.kt
+++ wui/src/main/kotlin/templating.kt
@@ -7,5 +7,5 @@ fun tmRender(templateId: String, data: Map<String, Any>): String =
 
 private fun render(template: String, data: Map<String, Any>): String {
     Mustache.parse(template)
-    return Mustache.render(template, dynamicToAny(mapToJsObject(data)))
+    return Mustache.render(template, dynamicToAny(data.toJsObj()))
 }
