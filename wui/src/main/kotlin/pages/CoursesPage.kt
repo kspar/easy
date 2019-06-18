fb4c5ea4cbb6454d31120e9857420f0a96167e10
Kaspar Papli <paplka@f-secure.com>
2019-06-18 17:28:59 +0300

    wui courses page templating poc

--- wui/src/main/kotlin/pages/CoursesPage.kt
+++ wui/src/main/kotlin/pages/CoursesPage.kt
@@ -1,13 +1,18 @@
 package pages
 
 import PageName
+import debug
 import debugFunEnd
 import debugFunStart
 import getElemById
+import objOf
 import spa.Page
-import kotlin.dom.appendText
+import tmRender
+import kotlin.js.Date
 
-object CoursesPage : Page<Nothing>() {
+object CoursesPage : Page<CoursesPage.State>() {
+
+    data class State(val coursesHtml: String)
 
     override val pageName: PageName
         get() = PageName.COURSES
@@ -15,12 +20,28 @@ object CoursesPage : Page<Nothing>() {
     override fun pathMatches(path: String): Boolean =
             path.matches("^/courses$")
 
-    override fun build(pageState: Nothing?) {
+    override fun build(pageState: State?) {
         val fl = debugFunStart("CoursesPage.build")
 
-        getElemById("container").appendText("courses")
+        val coursesListHtml = if (pageState != null) {
+            debug { "Got courses from state" }
+            pageState.coursesHtml
+        } else {
+            val html = genCourseListHtml()
+            updateState(State(html))
+            html
+        }
+
+        getElemById("container").innerHTML = coursesListHtml
 
         debugFunEnd(fl)
     }
+
+    private fun genCourseListHtml() = tmRender("tm-stud-course-list",
+            mapOf("course" to arrayOf(
+                    objOf("title" to "Title${Date().getMilliseconds()}", "id" to { 1 + 2 }),
+                    objOf("title" to "Title84", "id" to "42f")
+            )))
+
 
 }
