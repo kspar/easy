3d2698851df5f60980bc91767c68dd8a513548c8
kspar <kaspar.papli@gmail.com>
2019-06-25 16:30:28 +0300

    E-1005 ems db change student's and teacher's id (pk) to username

--- ems/src/main/kotlin/ee/urgas/ems/db/Tables.kt
+++ ems/src/main/kotlin/ee/urgas/ems/db/Tables.kt
@@ -7,21 +7,22 @@ import org.jetbrains.exposed.sql.Column
 
 
 object Teacher : IdTable<String>("teacher") {
-    override val id: Column<EntityID<String>> = text("email").primaryKey().entityId()
+    override val id: Column<EntityID<String>> = text("username").primaryKey().entityId()
     val createdAt = datetime("created_at")
+    val email = text("email")
     val givenName = text("given_name")
     val familyName = text("family_name")
 }
 
 object Exercise : LongIdTable("exercise") {
-    val owner = reference("owned_by", Teacher)
+    val owner = reference("owned_by_id", Teacher)
     val createdAt = datetime("created_at")
     val public = bool("public")
 }
 
 object ExerciseVer : LongIdTable("exercise_version") {
     val exercise = reference("exercise_id", Exercise)
-    val author = reference("author", Teacher)
+    val author = reference("author_id", Teacher)
     val previous = reference("previous_id", ExerciseVer).nullable()
     val validFrom = datetime("valid_from")
     val validTo = datetime("valid_to").nullable()
@@ -48,25 +49,26 @@ object CourseExercise : LongIdTable("course_exercise") {
 }
 
 object TeacherCourseAccess : LongIdTable("teacher_course_access") {
-    val teacher = reference("teacher_email", Teacher)
+    val teacher = reference("teacher_id", Teacher)
     val course = reference("course_id", Course)
 }
 
 object Student : IdTable<String>("student") {
-    override val id: Column<EntityID<String>> = text("email").primaryKey().entityId()
+    override val id: Column<EntityID<String>> = text("username").primaryKey().entityId()
     val createdAt = datetime("created_at")
+    val email = text("email")
     val givenName = text("given_name")
     val familyName = text("family_name")
 }
 
 object StudentCourseAccess : LongIdTable("student_course_access") {
-    val student = reference("student_email", Student)
+    val student = reference("student_id", Student)
     val course = reference("course_id", Course)
 }
 
 object Submission : LongIdTable("submission") {
     val courseExercise = reference("course_exercise_id", CourseExercise)
-    val student = reference("student_email", Student)
+    val student = reference("student_id", Student)
     val createdAt = datetime("created_at")
     val solution = text("solution")
     val autoGradeStatus = enumerationByName("auto_grade_status", 20, AutoGradeStatus::class)
@@ -74,7 +76,7 @@ object Submission : LongIdTable("submission") {
 
 object TeacherAssessment : LongIdTable("teacher_assessment") {
     val submission = reference("submission_id", Submission)
-    val teacher = reference("teacher_email", Teacher)
+    val teacher = reference("teacher_id", Teacher)
     val createdAt = datetime("created_at")
     val grade = integer("grade")
     val feedback = text("feedback").nullable()
