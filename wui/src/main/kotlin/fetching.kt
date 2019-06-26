17db7fbe8e0762f8a963250c5e853284e17129fa
kspar <kaspar.papli@gmail.com>
2019-06-26 21:56:59 +0300

    E-1001 wui update token before fetch

--- wui/src/main/kotlin/fetching.kt
+++ wui/src/main/kotlin/fetching.kt
@@ -4,7 +4,6 @@ import kotlin.browser.window
 import kotlin.js.Promise
 
 
-
 enum class ReqMethod {
     GET,
     POST
@@ -12,19 +11,26 @@ enum class ReqMethod {
 
 fun fetchEms(path: String, method: ReqMethod,
              data: Map<String, Any>? = null,
-             headers: Map<String, String> = emptyMap()): Promise<Response> {
+             headers: Map<String, String> = emptyMap()): Promise<Response> =
 
-    val defaultHeaders = mapOf(
-            "Authorization" to "Bearer ${Keycloak.token}",
-            "Content-Type" to "application/json")
+        Promise { resolve, reject ->
+            Keycloak.makeSureTokenIsValid()
+                    .then {
+                        val defaultHeaders = mapOf(
+                                "Authorization" to "Bearer ${Keycloak.token}",
+                                "Content-Type" to "application/json")
 
-    val combinedHeaders = defaultHeaders + headers
+                        val combinedHeaders = defaultHeaders + headers
 
-    val jsonData = if (data == null) "" else JSON.stringify(dynamicToAny(data.toJsObj()))
+                        val jsonData = if (data == null) "" else JSON.stringify(dynamicToAny(data.toJsObj()))
 
-    return window.fetch(AppProperties.EMS_ROOT + path,
-            RequestInit(
-                    method.name,
-                    combinedHeaders.toJsObj(),
-                    jsonData))
-}
+                        window.fetch(AppProperties.EMS_ROOT + path,
+                                RequestInit(
+                                        method.name,
+                                        combinedHeaders.toJsObj(),
+                                        jsonData))
+                                .then(resolve)
+                                .catch(reject)
+                    }
+                    .catch { reject }
+        }
