e1ebfbb7be510c8fb46f5414f81d426ef40797a0
kspar <kaspar.papli@gmail.com>
2019-06-26 11:49:57 +0300

    E-1004 ems account registering service to personal data updating service

--- ems/src/main/kotlin/ee/urgas/ems/bl/register/RegisterAccount.kt
+++ ems/src/main/kotlin/ee/urgas/ems/bl/register/RegisterAccount.kt
@@ -1,15 +1,16 @@
 package ee.urgas.ems.bl.register
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import ee.urgas.ems.conf.security.EasyUser
 import ee.urgas.ems.db.Student
 import ee.urgas.ems.db.Teacher
+import ee.urgas.ems.db.insertOrUpdate
 import mu.KotlinLogging
 import org.jetbrains.exposed.dao.EntityID
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -19,18 +20,23 @@ private val log = KotlinLogging.logger {}
 @RequestMapping("/v2")
 class RegisterAccountController {
 
-    // TODO: register -> update
-    @PostMapping("/register")
-    fun registerAccount(caller: EasyUser) {
+    data class PersonalDataBody(@JsonProperty("email", required = true) val email: String,
+                                @JsonProperty("first_name", required = true) val firstName: String,
+                                @JsonProperty("last_name", required = true) val lastName: String)
 
-        val account = Account(caller.id, caller.email, caller.givenName, caller.familyName)
-        log.debug { "Register $account" }
+    @PostMapping("/account/personal")
+    fun updateAccount(@RequestBody dto: PersonalDataBody, caller: EasyUser) {
+
+        val account = Account(caller.id, dto.email, dto.firstName, dto.lastName)
+        log.debug { "Update personal data for $account" }
 
         if (caller.isStudent()) {
-            registerStudent(account)
+            log.debug { "Update student ${caller.id}" }
+            updateStudent(account)
         }
         if (caller.isTeacher()) {
-            registerTeacher(account)
+            log.debug { "Update teacher ${caller.id}" }
+            updateTeacher(account)
         }
     }
 }
@@ -39,42 +45,10 @@ class RegisterAccountController {
 data class Account(val username: String, val email: String, val givenName: String, val familyName: String)
 
 
-private fun registerStudent(student: Account) {
-    if (!studentExists(student.username)) {
-        log.debug { "Student ${student.username} not found, inserting" }
-        insertStudent(student)
-    } else {
-        log.debug { "Student ${student.username} already exists" }
-    }
-}
-
-private fun registerTeacher(teacher: Account) {
-    if (!teacherExists(teacher.username)) {
-        log.debug { "Teacher ${teacher.username} not found, inserting" }
-        insertTeacher(teacher)
-    } else {
-        log.debug { "Teacher ${teacher.username} already exists" }
-    }
-}
-
-private fun studentExists(id: String): Boolean {
-    return transaction {
-        Student.select { Student.id eq id }
-                .count() == 1
-    }
-}
-
-private fun teacherExists(id: String): Boolean {
-    return transaction {
-        Teacher.select { Teacher.id eq id }
-                .count() == 1
-    }
-}
-
-private fun insertStudent(student: Account) {
+private fun updateStudent(student: Account) {
     transaction {
-        Student.insert {
-            it[id] = EntityID(student.email, Student)
+        Student.insertOrUpdate(Student.id, listOf(Student.id, Student.createdAt)) {
+            it[id] = EntityID(student.username, Student)
             it[email] = student.email
             it[givenName] = student.givenName
             it[familyName] = student.familyName
@@ -83,10 +57,10 @@ private fun insertStudent(student: Account) {
     }
 }
 
-private fun insertTeacher(teacher: Account) {
+private fun updateTeacher(teacher: Account) {
     transaction {
-        Teacher.insert {
-            it[id] = EntityID(teacher.email, Teacher)
+        Teacher.insertOrUpdate(Teacher.id, listOf(Teacher.id, Teacher.createdAt)) {
+            it[id] = EntityID(teacher.username, Teacher)
             it[email] = teacher.email
             it[givenName] = teacher.givenName
             it[familyName] = teacher.familyName
