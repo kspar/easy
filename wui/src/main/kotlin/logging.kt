5fc207da4baa0dab70eef589629e611b51c08dc3
Kaspar Papli <paplka@f-secure.com>
2019-07-03 17:42:55 +0300

    wui log levels now defined in app properties

--- wui/src/main/kotlin/logging.kt
+++ wui/src/main/kotlin/logging.kt
@@ -4,18 +4,15 @@ import kotlin.js.Date
 private const val DEBUG_PREFIX = "[DEBUG]"
 private const val WARN_PREFIX = "[WARN]"
 
-private const val DEBUG_ENABLED = true
-private const val WARN_ENABLED = true
-
 
 // No console.debug in kotlin/js...
 fun debug(msgProvider: () -> String) {
-    if (DEBUG_ENABLED)
+    if (AppProperties.LOG_DEBUG_ENABLED)
         console.log("$DEBUG_PREFIX ${datetimeString()}: ${msgProvider()}")
 }
 
 fun warn(msgProvider: () -> String) {
-    if (WARN_ENABLED)
+    if (AppProperties.LOG_WARN_ENABLED)
         console.warn("$WARN_PREFIX ${datetimeString()}: ${msgProvider()}")
 }
 
@@ -27,7 +24,7 @@ class FunLog(private val funName: String, private val funStartTime: Double) {
 }
 
 fun debugFunStart(funName: String): FunLog? {
-    if (DEBUG_ENABLED) {
+    if (AppProperties.LOG_DEBUG_ENABLED) {
         debug { "--> $funName" }
         return FunLog(funName, Date.now())
     }
