be105a96b7a82acf63ccee9150955b27ec6c9bf0
Kaspar Papli <paplka@f-secure.com>
2019-07-06 13:07:36 +0300

    ems basic course info service

--- ems/src/main/kotlin/ee/urgas/ems/bl/access/AccessControl.kt
+++ ems/src/main/kotlin/ee/urgas/ems/bl/access/AccessControl.kt
@@ -1,29 +1,41 @@
 package ee.urgas.ems.bl.access
 
+import ee.urgas.ems.conf.security.EasyUser
 import ee.urgas.ems.db.CourseExercise
 import ee.urgas.ems.db.StudentCourseAccess
-import ee.urgas.ems.db.Teacher
 import ee.urgas.ems.db.TeacherCourseAccess
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
 
+fun canUserAccessCourse(user: EasyUser, courseId: Long): Boolean =
+        when {
+            user.isAdmin() -> true
+            user.isTeacher() -> canTeacherAccessCourse(user.id, courseId)
+            user.isStudent() -> canStudentAccessCourse(user.id, courseId)
+            else -> throw IllegalStateException("User has no mapped roles: $user")
+        }
+
+
 fun canTeacherAccessCourse(teacherId: String, courseId: Long): Boolean {
     return transaction {
-        // TODO: remove join
-        (Teacher innerJoin TeacherCourseAccess)
-                .select { Teacher.id eq teacherId and (TeacherCourseAccess.course eq courseId) }
+        TeacherCourseAccess
+                .select {
+                    TeacherCourseAccess.teacher eq teacherId and
+                            (TeacherCourseAccess.course eq courseId)
+                }
                 .count() > 0
     }
 }
 
-
 fun canStudentAccessCourse(studentId: String, courseId: Long): Boolean {
     return transaction {
         StudentCourseAccess
-                .select { StudentCourseAccess.student eq studentId and
-                        (StudentCourseAccess.course eq courseId) }
+                .select {
+                    StudentCourseAccess.student eq studentId and
+                            (StudentCourseAccess.course eq courseId)
+                }
                 .count() > 0
     }
 }
---
+++ ems/src/main/kotlin/ee/urgas/ems/bl/course/ReadBasicCourseInfo.kt
@@ -0,0 +1,60 @@
+package ee.urgas.ems.bl.course
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import ee.urgas.ems.bl.access.canUserAccessCourse
+import ee.urgas.ems.conf.security.EasyUser
+import ee.urgas.ems.db.Course
+import ee.urgas.ems.exception.ForbiddenException
+import ee.urgas.ems.exception.InvalidRequestException
+import mu.KotlinLogging
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+@RequestMapping("/v2")
+class ReadBasicCourseInfo {
+
+    data class BasicCourseResponse(@JsonProperty("title") val title: String)
+
+    @Secured("ROLE_STUDENT", "ROLE_TEACHER", "ROLE_ADMIN")
+    @GetMapping("/courses/{courseId}/basic")
+    fun getCourseBasic(@PathVariable("courseId") courseIdStr: String,
+                       caller: EasyUser): BasicCourseResponse {
+
+        log.debug { "Getting basic course info for ${caller.id} for course $courseIdStr" }
+
+        val courseId = courseIdStr.toLongOrNull()
+                ?: throw InvalidRequestException("Course id is not a number")
+
+        if (!canUserAccessCourse(caller, courseId)) {
+            throw ForbiddenException("User ${caller.id} does not have access to course $courseId")
+        }
+
+        val courseInfo = selectCourseInfo(courseId)
+        log.debug { "Got course info: $courseInfo" }
+        return courseInfo
+    }
+
+    private fun selectCourseInfo(courseId: Long): BasicCourseResponse =
+            transaction {
+                Course.slice(Course.title)
+                        .select {
+                            Course.id eq courseId
+                        }
+                        .map {
+                            BasicCourseResponse(
+                                    it[Course.title]
+                            )
+                        }
+                        .single()
+            }
+}
+
+
