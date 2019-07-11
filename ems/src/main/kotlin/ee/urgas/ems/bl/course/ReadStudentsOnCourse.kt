f80a2d919dfe3126534f85e7f35940b630113b8d
Priit Paluoja <priitpaluoja@gmail.com>
2019-07-11 14:44:27 +0300

    E-1049 ems convert existing service to participants service

--- ems/src/main/kotlin/ee/urgas/ems/bl/course/ReadStudentsOnCourse.kt
+++ ems/src/main/kotlin/ee/urgas/ems/bl/course/ReadStudentsOnCourse.kt
@@ -1,64 +1,109 @@
 package ee.urgas.ems.bl.course
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.annotation.JsonProperty
 import ee.urgas.ems.bl.access.assertTeacherOrAdminHasAccessToCourse
 import ee.urgas.ems.bl.idToLongOrInvalidReq
 import ee.urgas.ems.conf.security.EasyUser
-import ee.urgas.ems.db.Course
-import ee.urgas.ems.db.Student
-import ee.urgas.ems.db.StudentCourseAccess
+import ee.urgas.ems.db.*
+import ee.urgas.ems.exception.InvalidRequestException
 import mu.KotlinLogging
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.access.annotation.Secured
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 private val log = KotlinLogging.logger {}
 
 @RestController
 @RequestMapping("/v2")
-class ReadStudentsOnCourseController {
+class ReadParticipantsOnCourseController {
 
     data class StudentsOnCourseResponse(@JsonProperty("id") val id: String,
+                                        @JsonProperty("email") val email: String,
                                         @JsonProperty("given_name") val givenName: String,
                                         @JsonProperty("family_name") val familyName: String)
 
-    @Secured("ROLE_TEACHER", "ROLE_ADMIN")
-    @GetMapping("/teacher/courses/{courseId}/students")
-    fun readStudentsOnCourse(@PathVariable("courseId") courseIdStr: String, caller: EasyUser):
-            List<StudentsOnCourseResponse> {
+    data class TeachersOnCourseResponse(@JsonProperty("id") val id: String,
+                                        @JsonProperty("email") val email: String,
+                                        @JsonProperty("given_name") val givenName: String,
+                                        @JsonProperty("family_name") val familyName: String)
 
-        log.debug { "Getting students on course for ${caller.id}" }
+    data class ParticipantOnCourseResponse(@JsonProperty("students")
+                                           @JsonInclude(Include.NON_NULL) val students: List<StudentsOnCourseResponse>?,
+                                           @JsonProperty("teachers")
+                                           @JsonInclude(Include.NON_NULL) val teachers: List<TeachersOnCourseResponse>?)
+
+
+    enum class Role {
+        TEACHER,
+        STUDENT,
+        ALL
+    }
+
+    @Secured("ROLE_TEACHER", "ROLE_ADMIN")
+    @GetMapping("/courses/{courseId}/participants")
+    fun readStudentsOnCourse(@PathVariable("courseId") courseIdStr: String,
+                             @RequestParam("role", required = false) roleReq: String?,
+                             caller: EasyUser): ParticipantOnCourseResponse {
+
+        log.debug { "Getting participants on course for ${caller.id} with role $roleReq" }
+
         val courseId = courseIdStr.idToLongOrInvalidReq()
 
         assertTeacherOrAdminHasAccessToCourse(caller, courseId)
 
-        val students = selectStudentsOnCourse(courseId)
-        log.debug { "Students on course $courseId: $students" }
+        when (roleReq) {
+            Role.TEACHER.name.toLowerCase() -> {
+                val teachers = selectTeachersOnCourse(courseId)
+                log.debug { "Teachers on course $courseId: $teachers" }
+                return ParticipantOnCourseResponse(null, mapToTeachersOnCourseResponse(teachers))
 
-        return mapToStudentsOnCourseResponse(students)
+            }
+
+            Role.STUDENT.name.toLowerCase() -> {
+                val students = selectStudentsOnCourse(courseId)
+                log.debug { "Students on course $courseId: $students" }
+                return ParticipantOnCourseResponse(mapToStudentsOnCourseResponse(students), null)
+            }
+
+            Role.ALL.name.toLowerCase(), null -> {
+                val students = selectStudentsOnCourse(courseId)
+                val teachers = selectTeachersOnCourse(courseId)
+
+                log.debug { "Students on course $courseId: $students" }
+                log.debug { "Teachers on course $courseId: $teachers" }
+
+                return ParticipantOnCourseResponse(
+                        mapToStudentsOnCourseResponse(students),
+                        mapToTeachersOnCourseResponse(teachers)
+                )
+            }
+
+            else -> throw InvalidRequestException("Invalid parameter $roleReq")
+        }
     }
 
     private fun mapToStudentsOnCourseResponse(students: List<StudentOnCourse>) =
-            students.map { StudentsOnCourseResponse(it.id, it.givenName, it.familyName) }
+            students.map { StudentsOnCourseResponse(it.id, it.email, it.givenName, it.familyName) }
+
+    private fun mapToTeachersOnCourseResponse(teachers: List<TeacherOnCourse>) =
+            teachers.map { TeachersOnCourseResponse(it.id, it.email, it.givenName, it.familyName) }
 }
 
-
-data class StudentOnCourse(val id: String, val givenName: String, val familyName: String)
-
+data class StudentOnCourse(val id: String, val email: String, val givenName: String, val familyName: String)
 
 private fun selectStudentsOnCourse(courseId: Long): List<StudentOnCourse> {
     return transaction {
         (Student innerJoin StudentCourseAccess innerJoin Course)
-                .slice(Student.id, Student.givenName, Student.familyName)
+                .slice(Student.id, Student.email, Student.givenName, Student.familyName)
                 .select { Course.id eq courseId }
                 .withDistinct()
                 .map {
                     StudentOnCourse(
                             it[Student.id].value,
+                            it[Student.email],
                             it[Student.givenName],
                             it[Student.familyName]
                     )
@@ -66,4 +111,21 @@ private fun selectStudentsOnCourse(courseId: Long): List<StudentOnCourse> {
     }
 }
 
+data class TeacherOnCourse(val id: String, val email: String, val givenName: String, val familyName: String)
 
+private fun selectTeachersOnCourse(courseId: Long): List<TeacherOnCourse> {
+    return transaction {
+        (Teacher innerJoin TeacherCourseAccess innerJoin Course)
+                .slice(Teacher.id, Teacher.email, Teacher.givenName, Teacher.familyName)
+                .select { Course.id eq courseId }
+                .withDistinct()
+                .map {
+                    TeacherOnCourse(
+                            it[Teacher.id].value,
+                            it[Teacher.email],
+                            it[Teacher.givenName],
+                            it[Teacher.familyName]
+                    )
+                }
+    }
+}
