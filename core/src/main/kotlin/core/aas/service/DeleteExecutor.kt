b9eea03a1e8cc8a3c874e467af9b785454e8dbdb
Kaspar Papli <paplka@f-secure.com>
2019-07-12 23:17:49 +0300

    E-1043 add executor services to core

---
+++ core/src/main/kotlin/core/aas/service/DeleteExecutor.kt
@@ -0,0 +1,27 @@
+package core.aas.service
+
+import core.db.Executor
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v2")
+class DeleteExecutorController {
+
+    @Secured("ROLE_ADMIN")
+    @DeleteMapping("/executors/{executorId}")
+    fun removeExecutor(@PathVariable("executorId") executorId: String) {
+        deleteExecutor(executorId.toLong())
+    }
+}
+
+private fun deleteExecutor(executorId: Long) {
+    return transaction {
+        Executor.deleteWhere { Executor.id eq executorId }
+    }
+}
