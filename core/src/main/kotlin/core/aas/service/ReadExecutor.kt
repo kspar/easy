0c78531b924f416b68f4f01ec752c3b9d4e8b3ed
Kaspar Papli <paplka@f-secure.com>
2019-07-14 00:32:54 +0300

    core refactor read executor service

--- core/src/main/kotlin/core/aas/service/ReadExecutor.kt
+++ core/src/main/kotlin/core/aas/service/ReadExecutor.kt
@@ -1,7 +1,9 @@
 package core.aas.service
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import core.conf.security.EasyUser
 import core.db.Executor
+import mu.KotlinLogging
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.access.annotation.Secured
@@ -9,35 +11,35 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+private val log = KotlinLogging.logger {}
+
+
 @RestController
 @RequestMapping("/v2")
-class ReadExecutorController {
+class ReadExecutorCont {
 
-    data class ReadExecutorResponse(@JsonProperty("id") val id: String,
-                                    @JsonProperty("name") val name: String,
-                                    @JsonProperty("base_url") val baseUrl: String,
-                                    @JsonProperty("load") val load: Int,
-                                    @JsonProperty("max_load") val maxLoad: Int)
+    data class Resp(
+            @JsonProperty("id") val id: String,
+            @JsonProperty("name") val name: String,
+            @JsonProperty("base_url") val baseUrl: String,
+            @JsonProperty("load") val load: Int,
+            @JsonProperty("max_load") val maxLoad: Int)
 
-    @Secured("ROLE_TEACHER")
+    @Secured("ROLE_TEACHER", "ROLE_ADMIN")
     @GetMapping("/executors")
-    fun readExecutor(): List<ReadExecutorResponse> {
-        val executors = selectAllExecutors()
-        return mapToReadExecutorResponses(executors)
+    fun controller(caller: EasyUser): List<Resp> {
+        log.debug { "Getting executors for ${caller.id}" }
+        return selectAllExecutors()
     }
-
-    private fun mapToReadExecutorResponses(executors: List<ReadExecutor>): List<ReadExecutorResponse> =
-            executors.map { ex -> ReadExecutorResponse(ex.id.toString(), ex.name, ex.baseUrl, ex.load, ex.maxLoad) }
 }
 
-private data class ReadExecutor(val id: Long, val name: String, val baseUrl: String, val load: Int, val maxLoad: Int)
 
-private fun selectAllExecutors(): List<ReadExecutor> {
+private fun selectAllExecutors(): List<ReadExecutorCont.Resp> {
     return transaction {
         Executor.selectAll().sortedBy { Executor.id }
                 .map {
-                    ReadExecutor(
-                            it[Executor.id].value,
+                    ReadExecutorCont.Resp(
+                            it[Executor.id].value.toString(),
                             it[Executor.name],
                             it[Executor.baseUrl],
                             it[Executor.load],
