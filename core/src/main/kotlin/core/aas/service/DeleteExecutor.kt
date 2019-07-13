7f0a33ff1b4a28aa6fab436359183c07a64e7df4
Kaspar Papli <paplka@f-secure.com>
2019-07-14 00:20:00 +0300

    core delete executor service should give 400 if no executor exists with this id

--- core/src/main/kotlin/core/aas/service/DeleteExecutor.kt
+++ core/src/main/kotlin/core/aas/service/DeleteExecutor.kt
@@ -1,7 +1,10 @@
 package core.aas.service
 
 import core.db.Executor
+import core.ems.service.idToLongOrInvalidReq
+import core.exception.InvalidRequestException
 import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -16,12 +19,20 @@ class DeleteExecutorController {
     @Secured("ROLE_ADMIN")
     @DeleteMapping("/executors/{executorId}")
     fun removeExecutor(@PathVariable("executorId") executorId: String) {
-        deleteExecutor(executorId.toLong())
+        deleteExecutor(executorId.idToLongOrInvalidReq())
     }
 }
 
 private fun deleteExecutor(executorId: Long) {
     return transaction {
-        Executor.deleteWhere { Executor.id eq executorId }
+        val executorExists =
+                Executor.select { Executor.id eq executorId }
+                        .count() == 1
+
+        if (executorExists) {
+            Executor.deleteWhere { Executor.id eq executorId }
+        } else {
+            throw InvalidRequestException("Executor with id $executorId not found")
+        }
     }
 }
