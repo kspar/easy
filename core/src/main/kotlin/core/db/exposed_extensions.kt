97a79f038e3a448b0cb26ddc31367d80004ad26a
Priit Paluoja <priitpaluoja@gmail.com>
2019-08-16 11:07:42 +0300

    EZ-1062 rm unused imports

--- core/src/main/kotlin/core/db/exposed_extensions.kt
+++ core/src/main/kotlin/core/db/exposed_extensions.kt
@@ -1,8 +1,8 @@
 package core.db
 
-import org.jetbrains.exposed.dao.EntityID
-import org.jetbrains.exposed.dao.IdTable
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.statements.InsertStatement
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 
