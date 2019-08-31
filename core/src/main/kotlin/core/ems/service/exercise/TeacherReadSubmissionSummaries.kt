2ad4ec87ffb8769242435a593a5e55540525d4aa
kspar <kaspar.papli@gmail.com>
2019-08-31 14:17:33 +0200

    EZ-1083 teacher submission summaries service returns all students on course

--- core/src/main/kotlin/core/ems/service/exercise/TeacherReadSubmissionSummaries.kt
+++ core/src/main/kotlin/core/ems/service/exercise/TeacherReadSubmissionSummaries.kt
@@ -8,9 +8,7 @@ import core.ems.service.access.assertTeacherOrAdminHasAccessToCourse
 import core.ems.service.idToLongOrInvalidReq
 import core.util.DateTimeSerializer
 import mu.KotlinLogging
-import org.jetbrains.exposed.sql.SortOrder
-import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
 import org.springframework.security.access.annotation.Secured
@@ -25,113 +23,98 @@ private val log = KotlinLogging.logger {}
 @RequestMapping("/v2")
 class TeacherReadSubmissionSummariesController {
 
-    data class SubmissionSummaryResp(@JsonProperty("student_id") val studentId: String,
-                                     @JsonProperty("given_name") val studentGivenName: String,
-                                     @JsonProperty("family_name") val studentFamilyName: String,
-                                     @JsonSerialize(using = DateTimeSerializer::class)
-                                     @JsonProperty("submission_time") val submissionTime: DateTime,
-                                     @JsonProperty("grade") val grade: Int?,
-                                     @JsonProperty("graded_by") val gradedBy: GraderType?)
+    data class Resp(@JsonProperty("student_id") val studentId: String,
+                    @JsonProperty("given_name") val studentGivenName: String,
+                    @JsonProperty("family_name") val studentFamilyName: String,
+                    @JsonSerialize(using = DateTimeSerializer::class)
+                    @JsonProperty("submission_time") val submissionTime: DateTime?,
+                    @JsonProperty("grade") val grade: Int?,
+                    @JsonProperty("graded_by") val gradedBy: GraderType?)
 
     @Secured("ROLE_TEACHER", "ROLE_ADMIN")
     @GetMapping("/teacher/courses/{courseId}/exercises/{courseExerciseId}/submissions/latest/students")
-    fun readSubmissionSummaries(@PathVariable("courseId") courseIdString: String,
-                                @PathVariable("courseExerciseId") courseExerciseIdString: String,
-                                caller: EasyUser): List<SubmissionSummaryResp> {
+    fun controller(@PathVariable("courseId") courseIdString: String,
+                   @PathVariable("courseExerciseId") courseExerciseIdString: String,
+                   caller: EasyUser): List<Resp> {
 
         log.debug { "Getting submission summaries for ${caller.id} on course exercise $courseExerciseIdString on course $courseIdString" }
         val courseId = courseIdString.idToLongOrInvalidReq()
 
         assertTeacherOrAdminHasAccessToCourse(caller, courseId)
 
-        return mapToSubmissionSummaryResp(
-                selectTeacherSubmissionSummaries(courseId, courseExerciseIdString.idToLongOrInvalidReq()))
+        return selectTeacherSubmissionSummaries(courseId, courseExerciseIdString.idToLongOrInvalidReq())
     }
-
-    private fun mapToSubmissionSummaryResp(submissions: List<TeacherSubmissionSummary>) =
-            submissions.map {
-                SubmissionSummaryResp(
-                        it.studentId, it.studentGivenName, it.studentFamilyName, it.submissionTime,
-                        it.grade, it.gradedBy
-                )
-            }
 }
 
-
-data class TeacherSubmissionSummary(val studentId: String, val studentGivenName: String, val studentFamilyName: String,
-                                    val submissionTime: DateTime, val grade: Int?, val gradedBy: GraderType?)
-
-
-private fun selectTeacherSubmissionSummaries(courseId: Long, courseExId: Long): List<TeacherSubmissionSummary> {
+private fun selectTeacherSubmissionSummaries(courseId: Long, courseExId: Long):
+        List<TeacherReadSubmissionSummariesController.Resp> {
     return transaction {
+        val maxSubTime = Submission.createdAt.max()
 
-        data class SubmissionPartial(val id: Long, val studentId: String, val createdAt: DateTime)
-
-        // student_id -> submission
-        val lastSubmissions = HashMap<String, SubmissionPartial>()
-
-        (Course innerJoin CourseExercise innerJoin Submission)
-                .slice(Course.id, CourseExercise.id, Submission.id, Submission.student, Submission.createdAt)
-                .select { Course.id eq courseId and (CourseExercise.id eq courseExId) }
-                .map {
-                    SubmissionPartial(
-                            it[Submission.id].value,
-                            it[Submission.student].value,
-                            it[Submission.createdAt]
-                    )
+        (StudentCourseAccess innerJoin Student leftJoin (Submission innerJoin CourseExercise))
+                .slice(Student.id, Student.givenName, Student.familyName, maxSubTime)
+                .select {
+                    // CourseExercise.id & CourseExercise.course are null when the student has no submission
+                    (CourseExercise.id eq courseExId or CourseExercise.id.isNull()) and
+                            (CourseExercise.course eq courseId or CourseExercise.course.isNull()) and
+                            (StudentCourseAccess.course eq courseId)
                 }
-                .forEach {
-                    val lastSub = lastSubmissions[it.studentId]
-                    if (lastSub == null || lastSub.createdAt.isBefore(it.createdAt)) {
-                        lastSubmissions[it.studentId] = it
+                .groupBy(Student.id, Student.givenName, Student.familyName)
+                .map { latestSubmission ->
+
+                    val latestSubTime = latestSubmission[maxSubTime]
+
+                    if (latestSubTime == null) {
+                        TeacherReadSubmissionSummariesController.Resp(
+                                latestSubmission[Student.id].value,
+                                latestSubmission[Student.givenName],
+                                latestSubmission[Student.familyName],
+                                null,
+                                null,
+                                null
+                        )
+                    } else {
+                        var gradedBy: GraderType? = null
+                        var grade = selectTeacherGrade(latestSubTime)
+
+                        if (grade != null) {
+                            gradedBy = GraderType.TEACHER
+                        } else {
+                            grade = selectAutoGrade(latestSubTime)
+                            if (grade != null) {
+                                gradedBy = GraderType.AUTO
+                            }
+                        }
+
+                        TeacherReadSubmissionSummariesController.Resp(
+                                latestSubmission[Student.id].value,
+                                latestSubmission[Student.givenName],
+                                latestSubmission[Student.familyName],
+                                latestSubTime,
+                                grade,
+                                gradedBy
+                        )
                     }
                 }
-
-        lastSubmissions.map { (studentId, sub) ->
-
-            val studentName = selectStudentName(studentId)
-
-            var gradedBy: GraderType? = null
-            var grade = selectTeacherGrade(sub.id)
-
-            if (grade != null) {
-                gradedBy = GraderType.TEACHER
-            } else {
-                grade = selectAutoGrade(sub.id)
-                if (grade != null) {
-                    gradedBy = GraderType.AUTO
-                }
-            }
-
-            TeacherSubmissionSummary(
-                    studentId,
-                    studentName.first,
-                    studentName.second,
-                    sub.createdAt,
-                    grade,
-                    gradedBy
-            )
-        }.sortedByDescending { it.submissionTime }
     }
 }
 
-private fun selectAutoGrade(submissionId: Long): Int? {
-    return AutomaticAssessment.select { AutomaticAssessment.submission eq submissionId }
+private fun selectAutoGrade(submissionTime: DateTime): Int? {
+    return (AutomaticAssessment innerJoin Submission)
+            .slice(AutomaticAssessment.grade)
+            .select { Submission.createdAt eq submissionTime }
             .orderBy(AutomaticAssessment.createdAt to SortOrder.DESC)
             .limit(1)
             .map { it[AutomaticAssessment.grade] }
             .firstOrNull()
 }
 
-private fun selectTeacherGrade(submissionId: Long): Int? {
-    return TeacherAssessment.select { TeacherAssessment.submission eq submissionId }
+private fun selectTeacherGrade(submissionTime: DateTime): Int? {
+    return (TeacherAssessment innerJoin Submission)
+            .slice(TeacherAssessment.grade)
+            .select { Submission.createdAt eq submissionTime }
             .orderBy(TeacherAssessment.createdAt to SortOrder.DESC)
             .limit(1)
             .map { it[TeacherAssessment.grade] }
             .firstOrNull()
 }
-
-private fun selectStudentName(studentId: String): Pair<String, String> =
-        Student.select { Student.id eq studentId }
-                .map { Pair(it[Student.givenName], it[Student.familyName]) }
-                .single()
