cc2a40394c1ae84cf5957c73cfe84891385b748b
Priit Paluoja <priitpaluoja@gmail.com>
2019-09-03 10:02:14 +0300

    EZ-1082 implement message read service

--- core/src/main/kotlin/core/ems/service/management/ReadManagementNotification.kt
+++ core/src/main/kotlin/core/ems/service/management/ReadManagementNotification.kt
@@ -1,2 +1,49 @@
 package core.ems.service.management
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import core.conf.security.EasyUser
+import core.db.ManagementNotification
+import mu.KotlinLogging
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+@RequestMapping("/v2")
+class ReadManagementNotificationsController {
+
+    data class Resp(@JsonProperty("messages")
+                    @JsonInclude(JsonInclude.Include.NON_NULL) val messages: List<MessageResp>)
+
+    data class MessageResp(@JsonProperty("id") val messageId: String,
+                           @JsonProperty("message") val message: String)
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/management/notifications")
+    fun controller(caller: EasyUser): Resp {
+
+        log.debug { "Getting system management notifications for ${caller.id}" }
+
+        return selectMessages()
+    }
+}
+
+private fun selectMessages(): ReadManagementNotificationsController.Resp {
+    return transaction {
+        ReadManagementNotificationsController.Resp(ManagementNotification.selectAll()
+                .orderBy(ManagementNotification.id, SortOrder.DESC)
+                .map {
+                    ReadManagementNotificationsController.MessageResp(
+                            it[ManagementNotification.id].value.toString(),
+                            it[ManagementNotification.message]
+                    )
+                })
+    }
+}
