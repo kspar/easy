234684aece0675b2298036a16a39f0458eba21fa
Priit Paluoja <priitpaluoja@gmail.com>
2019-09-03 10:01:17 +0300

    EZ-1082 implement message creation service

--- core/src/main/kotlin/core/ems/service/management/CreateManagementNotification.kt
+++ core/src/main/kotlin/core/ems/service/management/CreateManagementNotification.kt
@@ -1,2 +1,39 @@
 package core.ems.service.management
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import core.conf.security.EasyUser
+import core.db.ManagementNotification
+import mu.KotlinLogging
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+@RequestMapping("/v2")
+class CreateManagementNotificationsController {
+
+    data class Req(@JsonProperty("message", required = true) val message: String)
+
+    @Secured("ROLE_ADMIN")
+    @PostMapping("/management/notifications")
+    fun controller(@RequestBody dto: Req, caller: EasyUser) {
+
+        log.debug { "${caller.id} is creating new system management notification: $dto" }
+
+        insertMessage(dto)
+    }
+}
+
+private fun insertMessage(dto: CreateManagementNotificationsController.Req) {
+    transaction {
+        ManagementNotification.insert {
+            it[message] = dto.message
+        }
+    }
+}
