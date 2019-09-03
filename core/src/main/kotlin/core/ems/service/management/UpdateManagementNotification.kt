8d2c074a10b5ff3a0f99ab6e7b4205a6896e0659
Priit Paluoja <priitpaluoja@gmail.com>
2019-09-03 10:05:00 +0300

    EZ-1082 implement message update service

--- core/src/main/kotlin/core/ems/service/management/UpdateManagementNotification.kt
+++ core/src/main/kotlin/core/ems/service/management/UpdateManagementNotification.kt
@@ -1,2 +1,40 @@
 package core.ems.service.management
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import core.conf.security.EasyUser
+import core.db.ManagementNotification
+import core.ems.service.idToLongOrInvalidReq
+import mu.KotlinLogging
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
+import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.*
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+@RequestMapping("/v2")
+class UpdateManagementNotificationsController {
+
+    data class Req(@JsonProperty("message", required = true) val message: String)
+
+    @Secured("ROLE_ADMIN")
+    @PatchMapping("/management/notifications/{notificationId}")
+    fun controller(@PathVariable("notificationId") notificationIdStr: String,
+                   @RequestBody dto: Req, caller: EasyUser) {
+
+        val notificationId = notificationIdStr.idToLongOrInvalidReq()
+
+        log.debug { "${caller.id} is updating system management notification $notificationId with $dto" }
+
+        updateMessage(dto, notificationId)
+    }
+}
+
+private fun updateMessage(dto: UpdateManagementNotificationsController.Req, notificationId: Long) {
+    transaction {
+        ManagementNotification.update({ ManagementNotification.id eq notificationId }) {
+            it[message] = dto.message
+        }
+    }
+}
