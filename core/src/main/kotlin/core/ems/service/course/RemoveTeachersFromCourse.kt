06bdaa357fff84e3dcde0402f7f3d0d8e866051b
Priit Paluoja <priitpaluoja@gmail.com>
2019-09-04 14:55:09 +0300

    EZ-1051 refactor teacher remove from courses + update tests + update api

--- core/src/main/kotlin/core/ems/service/course/RemoveTeachersFromCourse.kt
+++ core/src/main/kotlin/core/ems/service/course/RemoveTeachersFromCourse.kt
@@ -13,6 +13,8 @@ import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+import javax.validation.constraints.Size
 
 private val log = KotlinLogging.logger {}
 
@@ -20,14 +22,14 @@ private val log = KotlinLogging.logger {}
 @RequestMapping("/v2")
 class RemoveTeachersFromCourseController {
 
-    data class Req(@JsonProperty("teacher_id") val teacherId: String)
-
+    data class Req(@JsonProperty("teachers") @field:Valid val teacherIds: List<TeacherIdReq>)
+    data class TeacherIdReq(@JsonProperty("teacher_id") @field:Size(min = 1, max = 100) val teacherId: String)
 
     @Secured("ROLE_ADMIN")
     @DeleteMapping("/courses/{courseId}/teachers")
-    fun removeTeachers(@PathVariable("courseId") courseIdStr: String,
-                       @RequestBody teachers: List<Req>,
-                       caller: EasyUser) {
+    fun controller(@PathVariable("courseId") courseIdStr: String,
+                   @Valid @RequestBody teachers: Req,
+                   caller: EasyUser) {
 
         log.debug { "Removing teachers $teachers from course $courseIdStr" }
 
@@ -38,9 +40,9 @@ class RemoveTeachersFromCourseController {
 }
 
 
-private fun deleteTeachersFromCourse(teachers: List<RemoveTeachersFromCourseController.Req>, courseId: Long) {
+private fun deleteTeachersFromCourse(teachers: RemoveTeachersFromCourseController.Req, courseId: Long) {
     transaction {
-        teachers.forEach { teacher ->
+        teachers.teacherIds.forEach { teacher ->
             val teacherExists =
                     Teacher.select { Teacher.id eq teacher.teacherId }
                             .count() == 1
@@ -49,7 +51,7 @@ private fun deleteTeachersFromCourse(teachers: List<RemoveTeachersFromCourseCont
             }
         }
 
-        val teachersWithAccess = teachers.filter {
+        val teachersWithAccess = teachers.teacherIds.filter {
             TeacherCourseAccess.select {
                 TeacherCourseAccess.teacher eq it.teacherId and (TeacherCourseAccess.course eq courseId)
             }.count() > 0
