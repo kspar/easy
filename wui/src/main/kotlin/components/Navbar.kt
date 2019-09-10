805f31cc000e3b50d26efd2bcbc28518bceebca9
kspar <kaspar.papli@gmail.com>
2019-09-10 12:23:11 +0200

    EZ-1009 paint role suffix for persisted non-main roles & refactor navbar role switching

--- wui/src/main/kotlin/components/Navbar.kt
+++ wui/src/main/kotlin/components/Navbar.kt
@@ -5,70 +5,57 @@ import Str
 import buildStatics
 import getBody
 import getElemById
-import getElemByIdAs
 import libheaders.Materialize
-import org.w3c.dom.HTMLAnchorElement
+import onVanillaClick
 import spa.PageManager
 import tmRender
 import toJsObj
-import kotlin.dom.clear
 
 object Navbar {
 
     fun build() {
-        val navHtml = tmRender("tm-navbar", mapOf(
+        val isMainRoleActive = Auth.isMainRoleActive()
+
+        getElemById("nav-wrap").innerHTML = tmRender("tm-navbar", mapOf(
                 "userName" to Auth.firstName,
+                "roleIndicator" to if (isMainRoleActive) null else Str.roleChangeStudentSuffix(),
                 "myCourses" to Str.topMenuCourses(),
                 "account" to Str.accountData(),
                 "logOut" to Str.logOut(),
                 "accountLink" to Auth.createAccountUrl(),
                 "logoutLink" to Auth.createLogoutUrl()))
-        getElemById("nav-wrap").innerHTML = navHtml
 
-        if (Auth.isMainRoleActive())
-            buildRoleChangeToStudentIfPossible()
-        else
-            buildRoleChangeBackToMainRole()
+        when {
+            Auth.canToggleRole() && isMainRoleActive -> buildChangeToStudent()
+            !isMainRoleActive -> buildChangeToMain()
+        }
 
         initProfileDropdown()
     }
 
-    private fun buildRoleChangeBackToMainRole() {
-        val roleToMainHtml = tmRender("tm-role-link", mapOf(
+    private fun buildChangeToMain() {
+        getElemById("role-wrap").innerHTML = tmRender("tm-role-link", mapOf(
                 "changeRole" to Str.roleChangeBack(),
                 "changeRoleId" to "role-link-main"
         ))
-        getElemById("role-wrap").innerHTML = roleToMainHtml
 
-        getElemByIdAs<HTMLAnchorElement>("role-link-main").onclick = {
-            buildStatics()
-            getElemById("profile-role").clear()
-            buildRoleChangeToStudentIfPossible()
-            initProfileDropdown()
+        getElemById("role-link-main").onVanillaClick(true) {
             Auth.switchRoleToMain()
+            buildStatics()
             PageManager.updatePage()
-            it
         }
     }
 
-    private fun buildRoleChangeToStudentIfPossible() {
-        if (Auth.canToggleRole()) {
-            val roleToStudentHtml = tmRender("tm-role-link", mapOf(
-                    "changeRole" to Str.roleChangeStudent(),
-                    "changeRoleId" to "role-link-student"
-            ))
+    private fun buildChangeToStudent() {
+        getElemById("role-wrap").innerHTML = tmRender("tm-role-link", mapOf(
+                "changeRole" to Str.roleChangeStudent(),
+                "changeRoleId" to "role-link-student"
+        ))
 
-            getElemById("role-wrap").innerHTML = roleToStudentHtml
-
-            getElemByIdAs<HTMLAnchorElement>("role-link-student").onclick = {
-                buildStatics()
-                getElemById("profile-role").textContent = Str.roleChangeStudentSuffix()
-                buildRoleChangeBackToMainRole()
-                initProfileDropdown()
-                Auth.switchRoleToStudent()
-                PageManager.updatePage()
-                it
-            }
+        getElemById("role-link-student").onVanillaClick(true) {
+            Auth.switchRoleToStudent()
+            buildStatics()
+            PageManager.updatePage()
         }
     }
 
