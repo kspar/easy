1218757443efb7f30218adb5db06e3f194c1b1e2
Priit Paluoja <priitpaluoja@gmail.com>
2019-09-17 15:31:30 +0300

    EZ-1099 initial services

---
+++ core/src/main/kotlin/core/ems/service/exercise/StudentSubmitDraft.kt
@@ -0,0 +1,70 @@
+package core.ems.service.exercise
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import core.conf.security.EasyUser
+import core.db.CourseExercise
+import core.db.Student
+import core.db.SubmissionDraft
+import core.ems.service.access.assertIsVisibleExerciseOnCourse
+import core.ems.service.access.assertStudentHasAccessToCourse
+import core.ems.service.idToLongOrInvalidReq
+import mu.KotlinLogging
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.sql.insertIgnore
+import org.jetbrains.exposed.sql.statements.StatementType
+import org.jetbrains.exposed.sql.statements.UpdateBuilder
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
+import org.joda.time.DateTime
+import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+import javax.validation.constraints.Size
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+@RequestMapping("/v2")
+class StudentSubmitDraftController {
+
+    data class Req(@JsonProperty("solution", required = true) @field:Size(max = 300000) val solution: String)
+
+    @Secured("ROLE_STUDENT")
+    @PostMapping("/student/courses/{courseId}/exercises/{courseExerciseId}/draft")
+    fun controller(@PathVariable("courseId") courseIdStr: String,
+                   @PathVariable("courseExerciseId") courseExIdStr: String,
+                   @Valid @RequestBody solutionBody: Req, caller: EasyUser) {
+
+        log.debug { "Creating new submission draft by ${caller.id} on course exercise $courseExIdStr on course $courseIdStr" }
+        val courseId = courseIdStr.idToLongOrInvalidReq()
+        val courseExId = courseExIdStr.idToLongOrInvalidReq()
+
+        assertStudentHasAccessToCourse(caller.id, courseId)
+        assertIsVisibleExerciseOnCourse(courseExId, courseId)
+
+        insertOrUpdateSubmissionDraft(courseExId, solutionBody.solution, caller.id)
+    }
+}
+
+
+private fun insertOrUpdateSubmissionDraft(courseExId: Long, submission: String, studentId: String) {
+    return transaction {
+        val time = DateTime.now()
+        SubmissionDraft.insertIgnore {
+            it[courseExercise] = EntityID(courseExId, CourseExercise)
+            it[student] = EntityID(studentId, Student)
+            it[createdAt] = time
+            it[solution] = submission
+        }
+
+        SubmissionDraft.update {
+            it[courseExercise] = EntityID(courseExId, CourseExercise)
+            it[student] = EntityID(studentId, Student)
+            it[createdAt] = time
+            it[solution] = submission
+        }
+
+    }
+}
+
+
