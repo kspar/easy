1dd71e7a827fc80025c9ed55c906680dc906f2e1
kspar <kaspar.papli@gmail.com>
2019-09-17 14:45:07 +0200

    exposed upsert extension for composite primary keys

--- core/src/main/kotlin/core/db/exposed_extensions.kt
+++ core/src/main/kotlin/core/db/exposed_extensions.kt
@@ -8,7 +8,13 @@ import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 // Waiting for exposed upsert: https://github.com/JetBrains/Exposed/issues/167
 fun <T : Table> T.insertOrUpdate(key: Column<*>, excluded: List<Column<*>>, body: T.(InsertStatement<Number>) -> Unit) =
-        InsertOrUpdate<Number>(this, key = key, excluded = excluded).apply {
+        InsertOrUpdate<Number>(this, keys = listOf(key), excluded = excluded).apply {
+            body(this)
+            execute(TransactionManager.current())
+        }
+
+fun <T : Table> T.insertOrUpdate(keys: List<Column<*>>, excluded: List<Column<*>>, body: T.(InsertStatement<Number>) -> Unit) =
+        InsertOrUpdate<Number>(this, keys = keys, excluded = excluded).apply {
             body(this)
             execute(TransactionManager.current())
         }
@@ -16,14 +22,15 @@ fun <T : Table> T.insertOrUpdate(key: Column<*>, excluded: List<Column<*>>, body
 class InsertOrUpdate<Key : Any>(
         table: Table,
         isIgnore: Boolean = false,
-        private val key: Column<*>,
+        private val keys: List<Column<*>>,
         private val excluded: List<Column<*>>
 ) : InsertStatement<Key>(table, isIgnore) {
     override fun prepareSQL(transaction: Transaction): String {
         val tm = TransactionManager.current()
         val updateCols = table.columns.minus(excluded)
         val updateSetter = updateCols.joinToString { "${tm.identity(it)} = EXCLUDED.${tm.identity(it)}" }
-        val onConflict = "ON CONFLICT (${tm.identity(key)}) DO UPDATE SET $updateSetter"
+        val keyColsStr = keys.joinToString { tm.identity(it) }
+        val onConflict = "ON CONFLICT ($keyColsStr) DO UPDATE SET $updateSetter"
         return "${super.prepareSQL(transaction)} $onConflict"
     }
 }
