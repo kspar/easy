02a50b4bcddc3227c8cf9f52d49aac2e0cb14821
Priit Paluoja <priitpaluoja@gmail.com>
2019-09-23 13:18:15 +0300

    EZ-1054 initial operable service

--- core/src/main/kotlin/core/ems/service/management/report_client_log.kt
+++ core/src/main/kotlin/core/ems/service/management/report_client_log.kt
@@ -3,10 +3,16 @@ package core.ems.service.management
 import com.fasterxml.jackson.annotation.JsonProperty
 import core.conf.SysConf.getProp
 import core.conf.security.EasyUser
+import core.db.Account
+import core.db.LogReport
 import core.ems.service.management.ReportLogController.ReportClientSysProp.EMAIL_LOG_LEVEL
 import core.ems.service.management.ReportLogController.ReportClientSysProp.NO_MAIL_CLIENT_REGEX
 import core.exception.InvalidRequestException
 import mu.KotlinLogging
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +22,6 @@ import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
 private val log = KotlinLogging.logger {}
-
 
 @RestController
 @RequestMapping("/v2")
@@ -47,20 +52,29 @@ class ReportLogController {
     @PostMapping("/management/log")
     fun controller(@Valid @RequestBody dto: Req, caller: EasyUser) {
 
+        when (dto.logLevel) {
+            LogLevel.DEBUG.paramValue -> insertLog(dto, caller, LogLevel.DEBUG)
+            LogLevel.INFO.paramValue -> insertLog(dto, caller, LogLevel.INFO)
+            LogLevel.WARN.paramValue -> insertLog(dto, caller, LogLevel.WARN)
+            LogLevel.ERROR.paramValue -> insertLog(dto, caller, LogLevel.ERROR)
+            else -> throw InvalidRequestException("Invalid log level ${dto.logLevel}")
+        }
+
         // In some cases, we want to send email based on log level (must be configurable). (minimal log level)
         val emailLevel = getProp(EMAIL_LOG_LEVEL.propKey)
         // In some cases, we want to send email based on regular expression that matches the client_id.
         val noLogIfMatch = getProp(NO_MAIL_CLIENT_REGEX.propKey)
+    }
+}
 
-
-
-        when (dto.logLevel) {
-            LogLevel.DEBUG.paramValue -> log.debug { dto }
-            LogLevel.INFO.paramValue -> log.info { dto }
-            LogLevel.WARN.paramValue -> log.warn { dto }
-            LogLevel.ERROR.paramValue -> log.error { dto }
-            else -> throw InvalidRequestException("Invalid log level ${dto.logLevel}")
+fun insertLog(dto: ReportLogController.Req, caller: EasyUser, level: ReportLogController.LogLevel) {
+    transaction {
+        LogReport.insert {
+            it[userId] = EntityID(caller.id, Account)
+            it[logTime] = DateTime.now()
+            it[logLevel] = level.paramValue
+            it[logMessage] = dto.logMessage
+            it[clientId] = dto.clientId
         }
-
     }
 }
