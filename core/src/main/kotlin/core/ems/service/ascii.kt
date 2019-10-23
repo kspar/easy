f87616c97f6ac882e7cf3ed49134a6980767e5ff
Priit Paluoja <priitpaluoja@gmail.com>
2019-10-23 13:42:31 +0300

    EZ-1171 PoC

---
+++ core/src/main/kotlin/core/ems/service/ascii.kt
@@ -0,0 +1,36 @@
+package core.ems.service
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import core.conf.security.EasyUser
+import mu.KotlinLogging
+import org.asciidoctor.Asciidoctor.Factory.create
+import org.asciidoctor.Attributes
+import org.asciidoctor.Options
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+@RequestMapping("/v2")
+class AsciiController {
+
+
+    data class ReqResp(@JsonProperty("content") val content: String)
+
+
+    @PostMapping("/test/ascii")
+    fun controller(@Valid @RequestBody dto: ReqResp, caller: EasyUser): ReqResp {
+        log.debug { "${caller.id} is testing asciidoc." }
+
+        val attributes = Attributes()
+        val options = Options(mapOf(Pair("attributes", attributes)))
+
+        val asciidoctor = create()
+        return ReqResp(asciidoctor.convert(dto.content, options))
+    }
+}
