42e99d0204a8e397b3bab42c0b0d057458e545c1
kspar <kaspar.papli@gmail.com>
2019-12-18 16:08:19 +0100

    EZ-1217 adoc links are opened in new tab/window by default

--- core/src/main/kotlin/core/ems/service/adoc_service.kt
+++ core/src/main/kotlin/core/ems/service/adoc_service.kt
@@ -1,17 +1,24 @@
 package core.ems.service
 
+import mu.KotlinLogging
 import org.asciidoctor.Asciidoctor
 import org.asciidoctor.Attributes
 import org.asciidoctor.Options
+import org.asciidoctor.ast.Document
+import org.asciidoctor.extension.Postprocessor
+import org.jsoup.Jsoup
 import org.springframework.stereotype.Service
 
+
+private val log = KotlinLogging.logger {}
 
 // Annotated as service for automatic Spring initialization on start-up for fast first-time service access
 @Service
 class AsciiService {
     private val doctor = Asciidoctor.Factory.create()
+    private val options = Options()
 
-    fun adocToHtml(content: String): String {
+    init {
         val attributes = Attributes()
         attributes.setSourceHighlighter("highlightjs")
 
@@ -22,10 +29,27 @@ class AsciiService {
         attributes.setAttribute("ni", "</span>")
         attributes.setAttribute("nohl", "<span class=\"codehl nohl\">")
         attributes.setAttribute("lhon", "</span>")
-
-        val options = Options()
         options.setAttributes(attributes)
 
+        doctor.javaExtensionRegistry().postprocessor(LinkExternaliserProcessor())
+    }
+
+    fun adocToHtml(content: String): String {
         return doctor.convert(content, options)
+    }
+}
+
+class LinkExternaliserProcessor : Postprocessor() {
+    override fun process(document: Document?, output: String?): String {
+        val t0 = System.currentTimeMillis()
+
+        val jdoc = Jsoup.parse(output, "UTF-8")
+        jdoc.getElementsByTag("a").forEach {
+            it.attr("target", "_blank").attr("rel", "noopener noreferrer")
+        }
+        val html = jdoc.body().html()
+
+        log.debug { "Postprocessing took ${System.currentTimeMillis() - t0} ms" }
+        return html
     }
 }
