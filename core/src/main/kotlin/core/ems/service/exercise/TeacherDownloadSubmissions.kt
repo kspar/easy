c1cf217a2fb4a20cc436e0cea4a89ad7806e0c3f
Priit Paluoja <priitpaluoja@gmail.com>
2020-01-31 11:33:41 +0200

    EZ-1274 implement group logic

--- core/src/main/kotlin/core/ems/service/exercise/TeacherDownloadSubmissions.kt
+++ core/src/main/kotlin/core/ems/service/exercise/TeacherDownloadSubmissions.kt
@@ -2,18 +2,14 @@ package core.ems.service.exercise
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import core.conf.security.EasyUser
-import core.db.Account
-import core.db.CourseExercise
-import core.db.Student
-import core.db.Submission
+import core.db.*
 import core.ems.service.assertTeacherHasAccessToCourse
+import core.ems.service.getTeacherRestrictedGroups
 import core.ems.service.idToLongOrInvalidReq
 import core.util.Zip
 import core.util.writeZipFile
 import mu.KotlinLogging
-import org.jetbrains.exposed.sql.SortOrder
-import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.*
@@ -49,24 +45,42 @@ class TeacherDownloadSubmissionsController {
         writeZipFile(req.courses.map { it.id.idToLongOrInvalidReq() }
                 .flatMap {
                     assertTeacherHasAccessToCourse(caller.id, it)
-                    selectSubmission(exerciseId, it)
+                    selectSubmission(exerciseId, it, caller.id)
                 }, response.outputStream)
     }
 }
 
-//TODO: groups
-private fun selectSubmission(exerciseId: Long, courseId: Long): List<Zip> {
+
+private fun selectSubmission(exerciseId: Long, courseId: Long, callerId: String): List<Zip> {
     return transaction {
-        ((Submission innerJoin CourseExercise) innerJoin (Student innerJoin Account))
-                .slice(Submission.solution, Account.givenName, Account.familyName)
-                .select { (CourseExercise.exercise eq exerciseId) and (CourseExercise.course eq courseId) }
+
+        val restrictedGroups = getTeacherRestrictedGroups(courseId, callerId)
+
+        Join((Submission innerJoin CourseExercise) innerJoin (Student innerJoin Account),
+                StudentCourseAccess leftJoin (StudentGroupAccess innerJoin Group),
+                onColumn = Submission.student, otherColumn = StudentCourseAccess.student)
+                .slice(Submission.solution,
+                        Account.givenName,
+                        Account.familyName,
+                        Group.name)
+                .select {
+                    CourseExercise.exercise eq exerciseId and
+                            (CourseExercise.course eq courseId) and
+                            (StudentGroupAccess.group inList restrictedGroups or StudentGroupAccess.group.isNull())
+                }
                 .orderBy(Submission.createdAt, SortOrder.DESC)
                 .distinctBy { Submission.student }
-                .map { Zip(it[Submission.solution], createFileName(it[Account.givenName], it[Account.familyName], courseId, "")) }
+                .map {
+                    Zip(it[Submission.solution],
+                            createFileName(it[Account.givenName],
+                                    it[Account.familyName],
+                                    courseId,
+                                    it[Group.name]))
+                }
     }
 }
 
-private fun createFileName(givenName: String, familyName: String, courseId: Long, group: String): String {
-    return "${givenName}_${familyName}_${courseId}_$group.py".replace(" ", "_")
+private fun createFileName(givenName: String, familyName: String, courseId: Long, group: String?): String {
+    return "${givenName}_${familyName}_${courseId}${if (group != null) "_$group" else ""}.py".replace(" ", "_")
 }
 
