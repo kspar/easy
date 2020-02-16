7565b0bc23f2c83cda93bf8eb886e45e3b33796a
Priit Paluoja <priitpaluoja@gmail.com>
2020-02-16 14:48:37 +0200

    EZ-1267 cache teacher, student and admin existence in account_checkin, add debug logging

--- core/src/main/kotlin/core/ems/service/PrivateCachingService.kt
+++ core/src/main/kotlin/core/ems/service/PrivateCachingService.kt
@@ -1,12 +1,15 @@
 package core.ems.service
 
 import core.db.*
+import mu.KotlinLogging
 import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import java.io.Serializable
+
+private val log = KotlinLogging.logger {}
 
 /**
  * Has to be a separate Spring component. Do not use directly - only through other Services.
@@ -17,6 +20,7 @@ class PrivateCachingService {
     @Cacheable("selectLatestValidGrades")
     @Suppress("SENSELESS_COMPARISON")
     fun selectLatestValidGradesAll(courseExerciseId: Long): List<Grade> {
+        log.debug { "$courseExerciseId not in 'selectLatestValidGrades' cache. Executing select." }
         return transaction {
             (Submission leftJoin TeacherAssessment leftJoin AutomaticAssessment)
                     .slice(Submission.id,
@@ -59,6 +63,7 @@ class PrivateCachingService {
 
     @Cacheable(value = ["account"], unless = "#result == null")
     fun selectAccount(username: String): Acc? {
+        log.debug { "$username not in 'account' cache. Executing select." }
         return transaction {
             Account.select { Account.id eq username }
                     .map {
@@ -70,6 +75,31 @@ class PrivateCachingService {
                                 it[Account.familyName]
                         )
                     }.singleOrNull()
+        }
+    }
+
+
+    @Cacheable(value = ["student"], unless = "#result == false")
+    fun studentExists(studentUsername: String): Boolean {
+        log.debug { "$studentUsername not in 'student' cache. Executing select." }
+        return transaction {
+            Student.select { Student.id eq studentUsername }.count() == 1
+        }
+    }
+
+    @Cacheable(value = ["teacher"], unless = "#result == false")
+    fun teacherExists(teacherUsername: String): Boolean {
+        log.debug { "$teacherUsername not in 'teacher' cache. Executing select." }
+        return transaction {
+            Teacher.select { Teacher.id eq teacherUsername }.count() == 1
+        }
+    }
+
+    @Cacheable(value = ["admin"], unless = "#result == false")
+    fun adminExists(adminUsername: String): Boolean {
+        log.debug { "$adminUsername not in 'admin' cache. Executing select." }
+        return transaction {
+            Admin.select { Admin.id eq adminUsername }.count() == 1
         }
     }
 }
