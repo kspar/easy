50c479d813cd0b1c128c46322f016be31f3c832f
Priit Paluoja <priitpaluoja@gmail.com>
2020-03-15 15:08:37 +0200

    EZ-1280 add new method for testing

--- core/src/main/kotlin/core/ems/service/code/CompareSourceCode.kt
+++ core/src/main/kotlin/core/ems/service/code/CompareSourceCode.kt
@@ -1,12 +1,13 @@
 package core.ems.service.code
 
+import me.xdrop.fuzzywuzzy.FuzzySearch
 import mu.KotlinLogging
 import java.util.*
 
 
 private val log = KotlinLogging.logger {}
 
-fun mainCommented() {
+fun mainTest() {
     val code1 = """
     s = 3
     for i in range(6):
@@ -48,10 +49,61 @@ fun mainCommented() {
             
     print(hello)
     """
+
+
+    val code4 = """
+    midagi = input()
+    muud = True and midagi or False
+    if midagi:
+        print(1)
+    print(2)
+    """
+
+    val code5 = """
+    midagi = input()
+    muud = midagi or True and True or True and False
+    if midagi and muud or True:
+        print(2)
+    print(1)
+    """
+
+
+    val code6 = """
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    print(1)
+    """
+
+    val code7 = """
+    print(1)
+    """
+
     val strategy = DiceCoefficientStrategy() // TODO: good
-    log.info { strategy.findClosestN(code1, listOf(code1, code2, code3), 3) }
+    //log.info { strategy.findClosestN(code1, listOf(code1, code2, code3), 3) }
+
+
+    val old = strategy.score(code1, code2)
+    val old2 = strategy.score(code1, code3)
+    val old3 = strategy.score(code4, code5)
+    val old4 = strategy.score(code6, code7)
+
+    val new = fuzzy(code1, code2)
+    val new2 = fuzzy(code1, code3)
+    val new3 = fuzzy(code4, code5)
+    val new4 = fuzzy(code6, code7)
+
+    log.info { "Similar $new vs $old" }
+    log.info { "Different $new2 vs $old2" }
+    log.info { "Different semantics $new3 vs $old3" }
+    log.info { "Print comparison: $new4 vs $old4" }
 }
 
+fun fuzzy(s1: String, s2: String): Int {
+    return FuzzySearch.ratio(s1, s2) //TODO: best
+}
 
 /**
  * Rewrote from: https://github.com/rrice/java-string-similarity
@@ -65,7 +117,7 @@ class DiceCoefficientStrategy {
                 .take(n)
     }
 
-    private fun score(first: String, second: String): Double {
+    fun score(first: String, second: String): Double {
         val s1 = splitIntoBigrams(first)
         val s2 = splitIntoBigrams(second)
 
