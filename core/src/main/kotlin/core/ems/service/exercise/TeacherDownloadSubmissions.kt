7c83475cf44219c4e7f1975e3bb24d9e3c8ab8ce
kspar <kaspar.papli@gmail.com>
2020-03-30 18:17:40 +0200

    EZ-1294 download submissions service takes optional group ids

--- core/src/main/kotlin/core/ems/service/exercise/TeacherDownloadSubmissions.kt
+++ core/src/main/kotlin/core/ems/service/exercise/TeacherDownloadSubmissions.kt
@@ -3,8 +3,8 @@ package core.ems.service.exercise
 import com.fasterxml.jackson.annotation.JsonProperty
 import core.conf.security.EasyUser
 import core.db.*
-import core.ems.service.assertTeacherHasAccessToCourse
 import core.ems.service.assertTeacherOrAdminHasAccessToCourse
+import core.ems.service.assertTeacherOrAdminHasAccessToCourseGroup
 import core.ems.service.getTeacherRestrictedGroups
 import core.ems.service.idToLongOrInvalidReq
 import core.util.Zip
@@ -26,7 +26,10 @@ class TeacherDownloadSubmissionsController {
 
     data class Req(@JsonProperty("courses") @field:NotEmpty val courses: List<CourseReq>)
 
-    data class CourseReq(@JsonProperty("id") val id: String)
+    data class CourseReq(@JsonProperty("id") val id: String,
+                         @JsonProperty("groups") val groups: List<GroupReq>?)
+
+    data class GroupReq(@JsonProperty("id") val id: String)
 
     @Secured("ROLE_TEACHER", "ROLE_ADMIN")
     @PostMapping("/export/exercises/{exerciseId}/submissions/latest")
@@ -39,26 +42,31 @@ class TeacherDownloadSubmissionsController {
 
         val exerciseId = exerciseIdStr.idToLongOrInvalidReq()
 
+        // Check access to courses and groups
+        req.courses.forEach { course ->
+            val courseId = course.id.idToLongOrInvalidReq()
+            assertTeacherOrAdminHasAccessToCourse(caller, courseId)
+            course.groups?.forEach {
+                assertTeacherOrAdminHasAccessToCourseGroup(caller, courseId, it.id.idToLongOrInvalidReq())
+            }
+        }
+
         response.contentType = "application/zip"
         response.setHeader("Content-disposition", "attachment; filename=submissions.zip")
 
-        // Check that 1) courses exists and 2) teacher has access to them
         writeZipFile(
-                req.courses.map {
-                    it.id.idToLongOrInvalidReq()
-                }.flatMap {
-                    assertTeacherOrAdminHasAccessToCourse(caller, it)
-                    selectSubmission(exerciseId, it, caller.id)
+                req.courses.map { course ->
+                    course.id.idToLongOrInvalidReq() to course.groups.orEmpty().map { it.id.idToLongOrInvalidReq() }
+                }.flatMap { (courseId, groupIds) ->
+                    selectSubmission(exerciseId, courseId, groupIds, caller.id)
                 },
                 response.outputStream)
     }
 }
 
 
-private fun selectSubmission(exerciseId: Long, courseId: Long, callerId: String): List<Zip> {
+private fun selectSubmission(exerciseId: Long, courseId: Long, groupIds: List<Long>, callerId: String): List<Zip> {
     return transaction {
-
-        val restrictedGroups = getTeacherRestrictedGroups(courseId, callerId)
 
         val join1 = Submission innerJoin CourseExercise innerJoin (Student innerJoin Account)
         val join2 = StudentCourseAccess leftJoin (StudentGroupAccess innerJoin Group)
@@ -76,9 +84,15 @@ private fun selectSubmission(exerciseId: Long, courseId: Long, callerId: String)
                 }
                 .orderBy(Submission.createdAt, SortOrder.DESC)
 
-        if (restrictedGroups.isNotEmpty()) {
-            query.andWhere {
-                StudentGroupAccess.group inList restrictedGroups or (StudentGroupAccess.group.isNull())
+        when {
+            groupIds.isNotEmpty() -> query.andWhere { StudentGroupAccess.group inList groupIds }
+            else -> {
+                val restrictedGroups = getTeacherRestrictedGroups(courseId, callerId)
+                if (restrictedGroups.isNotEmpty()) {
+                    query.andWhere {
+                        StudentGroupAccess.group inList restrictedGroups or (StudentGroupAccess.group.isNull())
+                    }
+                }
             }
         }
 
