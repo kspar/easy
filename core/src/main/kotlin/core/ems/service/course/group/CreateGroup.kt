a8901b307186a1aa6adcf5289bf711dd0a98b9d2
kspar <kaspar.papli@gmail.com>
2020-09-06 15:44:10 +0200

    EZ-1303 create group service

---
+++ core/src/main/kotlin/core/ems/service/course/group/CreateGroup.kt
@@ -0,0 +1,62 @@
+package core.ems.service.course.group
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import core.conf.security.EasyUser
+import core.db.Course
+import core.db.Group
+import core.ems.service.assertCourseExists
+import core.ems.service.assertTeacherOrAdminHasAccessToCourse
+import core.ems.service.idToLongOrInvalidReq
+import mu.KotlinLogging
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+@RequestMapping("/v2")
+class CreateGroupController {
+
+    data class Req(
+            @JsonProperty("name", required = true)
+            @field:NotBlank @field:Size(max = 200)
+            val name: String
+    )
+
+    data class Resp(
+            @JsonProperty("id") val id: String
+    )
+
+    @Secured("ROLE_TEACHER", "ROLE_ADMIN")
+    @PostMapping("/courses/{courseId}/groups")
+    fun controller(
+            @PathVariable("courseId") courseIdStr: String,
+            @Valid @RequestBody dto: Req,
+            caller: EasyUser
+    ): Resp {
+        log.debug { "Create group '${dto.name}' on course $courseIdStr by ${caller.id}" }
+
+        val courseId = courseIdStr.idToLongOrInvalidReq()
+        assertTeacherOrAdminHasAccessToCourse(caller, courseId)
+        assertCourseExists(courseId)
+
+        val groupId = deleteGroup(courseId, dto.name)
+        return Resp(groupId.toString())
+    }
+}
+
+
+private fun deleteGroup(courseId: Long, groupName: String): Long {
+    return transaction {
+        Group.insertAndGetId {
+            it[name] = groupName
+            it[course] = EntityID(courseId, Course)
+        }
+    }.value
+}
