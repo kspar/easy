45dc82c91554ea7bb91f97b897d9e6c85f5f6506
kspar <kaspar.papli@gmail.com>
2020-11-15 16:17:42 +0100

    EZ-1323 add color to create group service, add teacher as manager to created group

--- core/src/main/kotlin/core/ems/service/group/CreateGroup.kt
+++ core/src/main/kotlin/core/ems/service/group/CreateGroup.kt
@@ -1,7 +1,13 @@
 package core.ems.service.group
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import core.conf.security.EasyUser
+import core.db.Account
+import core.db.AccountGroup
 import core.db.Group
+import mu.KotlinLogging
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
@@ -14,30 +20,49 @@ import javax.validation.Valid
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
+private val log = KotlinLogging.logger {}
+
 @RestController
 @RequestMapping("/v2")
 class CreateGroupController {
-    data class Req(@JsonProperty("name", required = true) @field:NotBlank @field:Size(max = 100) val name: String)
+    data class Req(
+            @JsonProperty("name", required = true) @field:NotBlank @field:Size(max = 100) val name: String,
+            @JsonProperty("color", required = false) @field:Size(min = 1, max = 100) val color: String?
+    )
 
     data class Resp(@JsonProperty("id") val id: String)
 
     @Secured("ROLE_TEACHER", "ROLE_ADMIN")
     @PostMapping("/groups")
-    fun controller(@Valid @RequestBody body: Req): Resp {
-        val groupId = insertGroup(body)
+    fun controller(@Valid @RequestBody body: Req, caller: EasyUser): Resp {
+
+        log.debug { "Create group '${body.name}' (color: ${body.color}) by ${caller.id}" }
+
+        val groupId = insertGroup(body, caller)
         return Resp(groupId.toString())
     }
 }
 
-private fun insertGroup(newGroup: CreateGroupController.Req): Long {
+private fun insertGroup(newGroup: CreateGroupController.Req, caller: EasyUser): Long {
     return transaction {
-        // TODO: allow adding color
-        // TODO: if caller is teacher, add them to this group as manager
-        Group.insertAndGetId {
+        val now = DateTime.now()
+
+        val groupId = Group.insertAndGetId {
             it[name] = newGroup.name
-            it[color] = null
+            it[color] = newGroup.color
             it[isImplicit] = false
-            it[createdAt] = DateTime.now()
-        }.value
+            it[createdAt] = now
+        }
+
+        if (!caller.isAdmin()) {
+            AccountGroup.insert {
+                it[account] = EntityID(caller.id, Account)
+                it[group] = groupId
+                it[isManager] = true
+                it[createdAt] = now
+            }
+        }
+
+        groupId.value
     }
 }
