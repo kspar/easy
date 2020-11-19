7500152c7e5a7f6abf8cc0a7d4868f3c8477cee9
Kaspar Papli <kaspar@mnemonic.no>
2020-11-19 22:18:49 +0100

    EZ-1323 add debug lib printing function

--- core/src/main/kotlin/core/ems/service/dirs.kt
+++ core/src/main/kotlin/core/ems/service/dirs.kt
@@ -1,11 +1,18 @@
 package core.ems.service
 
 import core.db.Dir
+import core.db.DirAccessLevel
+import core.db.Group
+import core.db.GroupDirAccess
 import core.exception.InvalidRequestException
 import core.exception.ReqError
+import mu.KotlinLogging
 import org.jetbrains.exposed.sql.andWhere
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
+
+private val log = KotlinLogging.logger {}
+
 
 fun assertDirExists(dirId: Long, allowImplicit: Boolean = false) {
     if (!dirExists(dirId, allowImplicit)) {
@@ -25,5 +32,44 @@ fun dirExists(dirId: Long, allowImplicit: Boolean = false): Boolean {
         }
 
         q.count() == 1L
+    }
+}
+
+@Deprecated("For debugging only")
+fun debugPrintDir(dirId: Long? = null) {
+    log.warn { "For debugging only" }
+    printDir(dirId, 0)
+}
+
+private fun printDir(dirId: Long? = null, level: Int = 0) {
+    transaction {
+        Dir.select {
+            Dir.parentDir eq dirId
+        }.map {
+            val id = it[Dir.id].value
+            val name = it[Dir.name]
+            val anyAccess = if (it[Dir.anyAccess] != null) "(${it[Dir.anyAccess]})" else ""
+            val implicit = if (it[Dir.isImplicit]) "EXR" else "DIR"
+
+            data class GroupAccess(val groupId: Long, val groupName: String, val access: DirAccessLevel)
+
+            val accesses = (GroupDirAccess innerJoin Group)
+                    .select {
+                        GroupDirAccess.dir eq id
+                    }.map {
+                        GroupAccess(
+                                it[Group.id].value,
+                                it[Group.name],
+                                it[GroupDirAccess.level]
+                        )
+                    }
+
+            log.debug { "${" ".repeat(level * 4)}| $implicit: $name [$id] $anyAccess" }
+            accesses.forEach {
+                log.debug { "${" ".repeat(level * 4)}  Group ${it.groupName} [${it.groupId}] - ${it.access}" }
+            }
+
+            printDir(id, level + 1)
+        }
     }
 }
