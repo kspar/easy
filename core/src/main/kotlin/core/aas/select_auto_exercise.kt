a589bce69bb1d77f78789a03c318f510b8a66495
PriitPaluoja <priitpaluoja@gmail.com>
2021-10-19 15:19:39 +0300

    EZ-1336 refactor

--- core/src/main/kotlin/core/aas/select_auto_exercise.kt
+++ core/src/main/kotlin/core/aas/select_auto_exercise.kt
@@ -2,23 +2,22 @@ package core.aas
 
 import core.db.*
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.innerJoin
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
 
 data class AutoExerciseDetails(
-        val gradingScript: String,
-        val containerImage: String,
-        val maxTime: Int,
-        val maxMem: Int,
-        val assets: List<Pair<String, String>>,
-        val executors: List<AutoExerciseExecutorBasic>)
+    val gradingScript: String,
+    val containerImage: String,
+    val maxTime: Int,
+    val maxMem: Int,
+    val assets: List<Pair<String, String>>,
+    val executors: List<AutoExerciseExecutorBasic>
+)
 
 data class AutoExerciseExecutorBasic(
-        val id: Long,
-        val name: String
+    val id: Long,
+    val name: String
 )
 
 
@@ -26,28 +25,30 @@ fun selectAutoExercise(autoExerciseId: EntityID<Long>): AutoExerciseDetails {
     return transaction {
 
         val assets =
-                Asset.select { Asset.autoExercise eq autoExerciseId }
-                        .map { it[Asset.fileName] to it[Asset.fileContent] }
+            Asset.select { Asset.autoExercise eq autoExerciseId }
+                .map { it[Asset.fileName] to it[Asset.fileContent] }
 
         val executors =
             (AutoExercise innerJoin ContainerImage innerJoin ExecutorContainerImage innerJoin Executor)
-                .select { AutoExercise.id eq autoExerciseId}
-                        .map {
-                            AutoExerciseExecutorBasic(
-                                    it[Executor.id].value,
-                                    it[Executor.name])
-                        }
+                .select { AutoExercise.id eq autoExerciseId }
+                .map {
+                    AutoExerciseExecutorBasic(
+                        it[Executor.id].value,
+                        it[Executor.name]
+                    )
+                }
 
         AutoExercise.select {
             AutoExercise.id eq autoExerciseId
         }.map {
             AutoExerciseDetails(
-                    it[AutoExercise.gradingScript],
-                    it[AutoExercise.containerImage].value,
-                    it[AutoExercise.maxTime],
-                    it[AutoExercise.maxMem],
-                    assets,
-                    executors)
+                it[AutoExercise.gradingScript],
+                it[AutoExercise.containerImage].value,
+                it[AutoExercise.maxTime],
+                it[AutoExercise.maxMem],
+                assets,
+                executors
+            )
         }.single()
     }
 }
