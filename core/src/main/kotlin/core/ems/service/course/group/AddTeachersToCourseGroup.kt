9f074bd991583c12a38e18f4f34a0e669345e2df
Kaspar Papli <kaspar@mnemonic.no>
2021-10-22 16:53:03 +0200

    EZ-1305 refactor

--- core/src/main/kotlin/core/ems/service/course/group/AddTeachersToCourseGroup.kt
+++ core/src/main/kotlin/core/ems/service/course/group/AddTeachersToCourseGroup.kt
@@ -2,15 +2,11 @@ package core.ems.service.course.group
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import core.conf.security.EasyUser
-import core.db.Course
-import core.db.CourseGroup
-import core.db.Teacher
 import core.db.TeacherCourseGroup
 import core.ems.service.*
 import core.exception.InvalidRequestException
 import mu.KotlinLogging
-import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.sql.insertIgnore
+import org.jetbrains.exposed.sql.batchInsert
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.*
@@ -25,56 +21,51 @@ private val log = KotlinLogging.logger {}
 class AddTeachersToCourseGroupController {
 
     data class Req(
-            @JsonProperty("teachers")
-            @field:Valid
-            val teachers: List<TeacherReq>
+        @JsonProperty("teachers") @field:Valid val teachers: List<TeacherReq>
     )
 
     data class TeacherReq(
-            @JsonProperty("username")
-            @field:NotBlank @field:Size(max = 100)
-            val username: String
+        @JsonProperty("id") @field:NotBlank @field:Size(max = 100) val id: String
     )
 
 
     @Secured("ROLE_TEACHER", "ROLE_ADMIN")
     @PostMapping("/courses/{courseId}/groups/{groupId}/teachers")
     fun controller(
-            @PathVariable("courseId") courseIdStr: String,
-            @PathVariable("groupId") groupIdStr: String,
-            @Valid @RequestBody dto: Req,
-            caller: EasyUser
+        @PathVariable("courseId") courseIdStr: String,
+        @PathVariable("groupId") groupIdStr: String,
+        @Valid @RequestBody dto: Req,
+        caller: EasyUser
     ) {
-        log.debug { "Add teachers $dto to group $groupIdStr on course $courseIdStr by ${caller.id}" }
+        val teacherIds = dto.teachers.map { it.id }
+        log.debug { "Add teachers $teacherIds to group $groupIdStr on course $courseIdStr by ${caller.id}" }
 
         val courseId = courseIdStr.idToLongOrInvalidReq()
         val groupId = groupIdStr.idToLongOrInvalidReq()
 
-        /*
-        A teacher can only change other teachers' groups if they don't have any restricted groups themselves.
-        This makes it simpler and prevents privesc scenarios where a teacher can remove other teachers' restricted
-        groups to give them more capabilities than they themselves have.
-         */
+        if (teacherIds.contains(caller.id)) {
+            throw InvalidRequestException("You cannot add yourself to a group")
+        }
         assertTeacherOrAdminHasAccessToCourse(caller, courseId)
         assertTeacherOrAdminHasNoRestrictedGroupsOnCourse(caller, courseId)
         assertGroupExistsOnCourse(groupId, courseId)
 
-        addTeachersToGroup(courseId, groupId, dto.teachers.map { it.username })
+        teacherIds.forEach {
+            if (!canTeacherAccessCourse(it, courseId)) {
+                throw InvalidRequestException("Teacher $it does not have access to course $courseId")
+            }
+        }
+
+        addTeachersToGroup(courseId, groupId, teacherIds)
     }
 }
 
-private fun addTeachersToGroup(courseId: Long, groupId: Long, teacherUsernames: List<String>) {
+private fun addTeachersToGroup(courseId: Long, groupId: Long, teacherIds: List<String>) {
     transaction {
-        teacherUsernames.forEach { teacherUsername ->
-            if (!canTeacherAccessCourse(teacherUsername, courseId)) {
-                throw InvalidRequestException("Teacher $teacherUsername does not have access to course $courseId")
-            }
-
-            TeacherCourseGroup.insertIgnore {
-                it[teacher] = EntityID(teacherUsername, Teacher)
-                it[course] = EntityID(courseId, Course)
-                it[courseGroup] = EntityID(groupId, CourseGroup)
-            }
+        TeacherCourseGroup.batchInsert(teacherIds, ignore = true) {
+            this[TeacherCourseGroup.teacher] = it
+            this[TeacherCourseGroup.courseGroup] = groupId
+            this[TeacherCourseGroup.course] = courseId
         }
     }
 }
