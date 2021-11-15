13a53fb556f495a272ff75a2e642a46563dac211
PriitPaluoja <priitpaluoja@gmail.com>
2021-11-15 13:56:38 +0200

    EZ-1336 refactor AutoGradeStatusObserver.kt

--- core/src/main/kotlin/core/aas/AutoGradeStatusObserver.kt
+++ core/src/main/kotlin/core/aas/AutoGradeStatusObserver.kt
@@ -15,24 +15,20 @@ enum class ObserverCallerType {
 
 @Service
 class AutoAssessStatusObserver {
-    private data class StatusObserverKey(val submissionId: Long, val callerType: ObserverCallerType)
+    private data class KeyPair(val submissionId: Long, val callerType: ObserverCallerType)
 
-    private val statuses: MutableMap<StatusObserverKey, Job> = ConcurrentHashMap()
-
-    @Synchronized
-    private fun get(key: StatusObserverKey) = statuses[key]
+    private val statuses: MutableMap<KeyPair, Job> = ConcurrentHashMap()
 
     @Synchronized
-    private fun put(key: StatusObserverKey, value: Job) = statuses.put(key, value)
-
     fun put(submissionId: Long, callerType: ObserverCallerType, value: Job): Job? {
         log.debug { "Submission '$submissionId' by $callerType added to auto-assess status observer." }
-        return put(StatusObserverKey(submissionId, callerType), value)
+        return statuses.put(KeyPair(submissionId, callerType), value)
     }
 
+    @Synchronized
     fun get(submissionId: Long, callerType: ObserverCallerType): Job? {
         log.debug { "Submission '$submissionId' by $callerType retrieved from status observer." }
-        return get(StatusObserverKey(submissionId, callerType))
+        return statuses[KeyPair(submissionId, callerType)]
     }
 
     @Scheduled(fixedDelayString = "\${easy.core.auto-assess.fixed-delay-observer-clear.ms}")
