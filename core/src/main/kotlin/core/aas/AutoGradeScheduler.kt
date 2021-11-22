d55802915ce24294d5217f30b16014318e0ef1c2
PriitPaluoja <priitpaluoja@gmail.com>
2021-11-22 14:34:48 +0200

    EZ-1336 use @Synchronized annotation to reduce indentation

--- core/src/main/kotlin/core/aas/AutoGradeScheduler.kt
+++ core/src/main/kotlin/core/aas/AutoGradeScheduler.kt
@@ -25,26 +25,16 @@ private val log = KotlinLogging.logger {}
 @Service
 class AutoGradeScheduler : ApplicationListener<ContextRefreshedEvent> {
 
-    /**
-     * [submitAndAwait] expects that map of [executors] is synced with database. However [addExecutorsFromDB] can be called
-     * anytime, therefore [executorLock] is used to synchronize [submitAndAwait] and [addExecutorsFromDB] to avoid
-     * cases where [submitAndAwait] expects to see an executor in [executors] that otherwise might be already in db, but
-     * not in map.
-     */
-    private val executorLock = UUID.randomUUID().toString()
-
     private val executors: MutableMap<Long, SortedMap<PriorityLevel, FunctionScheduler<AutoAssessment>>> =
         ConcurrentHashMap()
 
     // Global index for picking the next queue
     private var queuePickerIndex = AtomicInteger()
 
+    @Synchronized
     override fun onApplicationEvent(p0: ContextRefreshedEvent) {
         log.info { "Initializing ${javaClass.simpleName} by syncing executors." }
-        // Synchronization might not be needed here, but for code consistency and for avoidance of thread issues is added.
-        synchronized(executorLock) {
-            addExecutorsFromDB()
-        }
+        addExecutorsFromDB()
     }
 
     /**
@@ -76,12 +66,11 @@ class AutoGradeScheduler : ApplicationListener<ContextRefreshedEvent> {
 
     //  fixedDelay doesn't start a next call before the last one has finished
     @Scheduled(fixedDelayString = "\${easy.core.auto-assess.fixed-delay.ms}")
+    @Synchronized
     private fun grade() {
         log.debug { "Grading executors $executors" }
         // Synchronized as executors can be removed or added at any time.
-        synchronized(executorLock) {
-            executors.keys.forEach { executorId -> gradeExecutor(executorId) }
-        }
+        executors.keys.forEach { executorId -> gradeExecutor(executorId) }
     }
 
 
@@ -99,7 +88,7 @@ class AutoGradeScheduler : ApplicationListener<ContextRefreshedEvent> {
         log.debug { "Scheduling and waiting for priority '$priority' autoExerciseId '$autoExerciseId'." }
 
         // Synchronized as executors can be removed or added at any time.
-        val executor = synchronized(executorLock) {
+        val executor = synchronized(this) {
             executors
                 .getOrElse(targetExecutor.id) {
                     throw ExecutorException("Out of sync. Did you use API to add/remove executor '${targetExecutor.id}'?")
@@ -114,49 +103,46 @@ class AutoGradeScheduler : ApplicationListener<ContextRefreshedEvent> {
     /**
      *  Remove executor.
      */
+    @Synchronized
     fun deleteExecutor(executorId: Long, force: Boolean) {
         // Synchronized as executors can be read or added at any time.
-        synchronized(executorLock) {
 
-            return transaction {
-                val executorQuery = Executor.select { Executor.id eq executorId }
-                val executorExists = executorQuery.count() == 1L
-                // The load of the all the (priority) queues in the executor map of executor x.
-                val currentLoad = executors[executorId]?.values?.sumOf { it.size() }
+        return transaction {
+            val executorQuery = Executor.select { Executor.id eq executorId }
+            val executorExists = executorQuery.count() == 1L
+            // The load of the all the (priority) queues in the executor map of executor x.
+            val currentLoad = executors[executorId]?.values?.sumOf { it.size() }
 
-                if (!executorExists) {
-                    throw InvalidRequestException("Executor with id $executorId not found")
+            if (!executorExists) {
+                throw InvalidRequestException("Executor with id $executorId not found")
 
-                } else if (!force && (currentLoad ?: 0) > 0) {
-                    throw InvalidRequestException("Executor load != 0 (is $currentLoad). Set 'force'=true for forced removal.")
+            } else if (!force && (currentLoad ?: 0) > 0) {
+                throw InvalidRequestException("Executor load != 0 (is $currentLoad). Set 'force'=true for forced removal.")
 
-                } else {
-                    ExecutorContainerImage.deleteWhere { ExecutorContainerImage.executor eq executorId }
-                    Executor.deleteWhere { Executor.id eq executorId }
-                    executors.remove(executorId)
-                    log.info { "Executor '$executorId' deleted" }
-                }
+            } else {
+                ExecutorContainerImage.deleteWhere { ExecutorContainerImage.executor eq executorId }
+                Executor.deleteWhere { Executor.id eq executorId }
+                executors.remove(executorId)
+                log.info { "Executor '$executorId' deleted" }
             }
         }
     }
 
-
+    @Synchronized
     fun addExecutorsFromDB() {
         // Synchronized as executors can be removed or read at any time.
-        synchronized(executorLock) {
-            val countBefore = executors.size
+        val countBefore = executors.size
 
-            getAvailableExecutorIds().forEach {
-                executors.putIfAbsent(
-                    // sortedMap does not need to be concurrent as all usages of this map are synchronized
-                    it, sortedMapOf(
-                        PriorityLevel.AUTHENTICATED to FunctionScheduler(::callExecutor),
-                        PriorityLevel.ANONYMOUS to FunctionScheduler(::callExecutor)
-                    )
+        getAvailableExecutorIds().forEach {
+            executors.putIfAbsent(
+                // sortedMap does not need to be concurrent as all usages of this map are synchronized
+                it, sortedMapOf(
+                    PriorityLevel.AUTHENTICATED to FunctionScheduler(::callExecutor),
+                    PriorityLevel.ANONYMOUS to FunctionScheduler(::callExecutor)
                 )
-            }
-
-            log.debug { "Checked for new executors. Executor count is now: $countBefore -> ${executors.size}." }
+            )
         }
+
+        log.debug { "Checked for new executors. Executor count is now: $countBefore -> ${executors.size}." }
     }
 }
