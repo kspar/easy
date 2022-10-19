15a4659221e2cc7eb10477e000f67e070fa35906
kspar <kaspar@mnemonic.no>
2022-10-19 11:36:01 +0200

    EZ-1407 refactor service names/files

--- core/src/main/kotlin/core/ems/service/exercise/ReadLatestTeacherSubmissions.kt
+++ core/src/main/kotlin/core/ems/service/exercise/ReadLatestTeacherSubmissions.kt
@@ -20,56 +20,59 @@ private val log = KotlinLogging.logger {}
 
 @RestController
 @RequestMapping("/v2")
-class TeacherReadLatestTeacherSubmissionController {
+class ReadLatestTeacherSubmissions {
 
-    data class RespSubmission(@JsonProperty("id") val id: String,
-                              @JsonProperty("solution") val solution: String,
-                              @JsonSerialize(using = DateTimeSerializer::class)
-                              @JsonProperty("created_at") val submissionTime: DateTime)
+    data class RespSubmission(
+        @JsonProperty("id") val id: String,
+        @JsonProperty("solution") val solution: String,
+        @JsonSerialize(using = DateTimeSerializer::class)
+        @JsonProperty("created_at") val submissionTime: DateTime
+    )
 
-
-    data class Resp(@JsonProperty("count") val count: Int,
-                    @JsonProperty("submissions") val submissions: List<RespSubmission>)
+    data class Resp(
+        @JsonProperty("count") val count: Int,
+        @JsonProperty("submissions") val submissions: List<RespSubmission>
+    )
 
 
     @Secured("ROLE_TEACHER", "ROLE_ADMIN")
     @GetMapping("/exercises/{exerciseId}/testing/autoassess/submissions")
-    fun controller(@PathVariable("exerciseId") exerciseIdStr: String,
-                   response: HttpServletResponse,
-                   @RequestParam("offset", required = false) offsetStr: String?,
-                   @RequestParam("limit", required = false) limitStr: String?,
-                   caller: EasyUser): Resp? {
+    fun controller(
+        @PathVariable("exerciseId") exerciseIdStr: String,
+        response: HttpServletResponse,
+        @RequestParam("offset", required = false) offsetStr: String?,
+        @RequestParam("limit", required = false) limitStr: String?,
+        caller: EasyUser
+    ): Resp? {
 
         log.debug { "Getting latest teacher submissions for ${caller.id} on exercise $exerciseIdStr" }
 
         val exerciseId = exerciseIdStr.idToLongOrInvalidReq()
         return selectLatestTeacherSubmissions(exerciseId, caller.id, offsetStr?.toLongOrNull(), limitStr?.toIntOrNull())
     }
-}
 
-
-private fun selectLatestTeacherSubmissions(exerciseId: Long, teacherId: String, offset: Long?, limit: Int?): TeacherReadLatestTeacherSubmissionController.Resp {
-    return transaction {
-        val selectQuery = TeacherSubmission
+    private fun selectLatestTeacherSubmissions(exerciseId: Long, teacherId: String, offset: Long?, limit: Int?): Resp {
+        return transaction {
+            val selectQuery = TeacherSubmission
                 .select {
                     (TeacherSubmission.exercise eq exerciseId) and
                             (TeacherSubmission.teacher eq teacherId)
                 }
                 .orderBy(TeacherSubmission.createdAt to SortOrder.DESC)
 
-        val totalSubmissions = selectQuery.count()
+            val totalSubmissions = selectQuery.count()
 
-        val submissions = selectQuery
+            val submissions = selectQuery
                 .limit(limit ?: totalSubmissions.toInt(), offset ?: 0)
                 .map {
-                    TeacherReadLatestTeacherSubmissionController.RespSubmission(
-                            it[TeacherSubmission.id].value.toString(),
-                            it[TeacherSubmission.solution],
-                            it[TeacherSubmission.createdAt])
+                    RespSubmission(
+                        it[TeacherSubmission.id].value.toString(),
+                        it[TeacherSubmission.solution],
+                        it[TeacherSubmission.createdAt]
+                    )
                 }
 
-        TeacherReadLatestTeacherSubmissionController.Resp(submissions.size, submissions)
+            Resp(submissions.size, submissions)
+        }
     }
 }
-
-
