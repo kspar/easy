ff7607455eaedf02d5bbbc7845ff85f78e5606f4
kspar <kaspar@mnemonic.no>
2022-11-24 12:04:08 +0100

    EZ-1478 tsl compile service

---
+++ core/src/main/kotlin/core/ems/service/exercise/CompileTSL.kt
@@ -0,0 +1,56 @@
+package core.ems.service.exercise
+
+import com.example.demo.compileTSL
+import com.fasterxml.jackson.annotation.JsonProperty
+import core.conf.security.EasyUser
+import core.exception.InvalidRequestException
+import core.exception.ReqError
+import mu.KotlinLogging
+import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+import javax.validation.constraints.Size
+
+@RestController
+@RequestMapping("/v2")
+class CompileTSL {
+    private val log = KotlinLogging.logger {}
+
+    data class Req(
+        @JsonProperty("tsl_spec") @field:Size(max = 100_000) val tslSpec: String,
+    )
+
+    data class Resp(
+        @JsonProperty("scripts") val scripts: List<ScriptResp>,
+    )
+
+    data class ScriptResp(
+        @JsonProperty("name") val name: String,
+        @JsonProperty("value") val value: String,
+    )
+
+    @Secured("ROLE_TEACHER", "ROLE_ADMIN")
+    @PostMapping("/tsl/compile")
+    fun controller(@Valid @RequestBody dto: Req, caller: EasyUser): Resp {
+        log.debug { "Compile TSL by ${caller.id}" }
+
+        val result = try {
+            compileTSL(dto.tslSpec, "1", "tiivad")
+
+        } catch (e: Exception) {
+            throw InvalidRequestException(
+                "TSL compilation failed with exception: ${e.message}",
+                ReqError.TSL_COMPILE_FAILED, notify = false
+            )
+        }
+
+        val resp = Resp(result.generatedScripts.mapIndexed { i, s ->
+            ScriptResp("generated$i.py", s)
+        })
+
+        return resp
+    }
+}
