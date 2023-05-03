01c4411a843a769aab3d26bd892715b27e5b7692
kspar <kaspar@mnemonic.no>
2023-05-03 10:56:17 +0200

    wui lib exercise page attrs style

--- wui/src/main/kotlin/components/text/AttrsComp.kt
+++ wui/src/main/kotlin/components/text/AttrsComp.kt
@@ -1,15 +1,19 @@
 package components.text
 
 import rip.kspar.ezspa.Component
-import tmRender
+import template
 
 class AttrsComp(
     val attrs: Map<String, String>,
     parent: Component,
 ) : Component(parent) {
 
-    override fun render() = tmRender(
-        "t-c-attrs",
+    override fun render() = template(
+        """
+            {{#attrs}}
+                <ez-attr><ez-attr-label>{{label}}:</ez-attr-label>{{value}}</ez-attr>
+            {{/attrs}}
+        """.trimIndent(),
         "attrs" to attrs.map {
             mapOf(
                 "label" to it.key,
