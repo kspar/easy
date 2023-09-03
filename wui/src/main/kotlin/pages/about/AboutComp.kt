d91f426c0177f26b0ed3198908d795c6be119ca4
kspar <kaspar@mnemonic.no>
2023-09-03 23:04:45 +0200

    wui about page update sponsors and text

--- wui/src/main/kotlin/pages/about/AboutComp.kt
+++ wui/src/main/kotlin/pages/about/AboutComp.kt
@@ -12,14 +12,19 @@ class AboutComp : Component(null, CONTENT_CONTAINER_ID) {
         """
             <ez-about>
                 <h2>Lahendusest</h2>
-                <p>Lahenduse keskkonda haldab ja arendab Tartu Ülikooli arvutiteaduse instituut. 
+                <p>Lahenduse keskkonda haldab ja arendab <a href='https://cs.ut.ee/et' target='_blank'>Tartu Ülikooli arvutiteaduse instituut</a>. 
                 Lahendus põhineb vabavaralisel rakendusel <a href='${AppProperties.REPO_URL}' target='_blank'>easy</a>, 
                 mida arendatakse samuti arvutiteaduse instituudis.</p>
                 <p>Kui sul on Lahenduse kasutamise või arenduse kohta küsimusi, või kui leidsid kuskilt vea, 
                 siis tule räägi sellest <a href='{{discordUrl}}${AppProperties.DISCORD_INVITE_ID}' target='_blank'>Lahenduse Discordi serveris</a>.</p> 
                 
                 <p style='margin-bottom: 3rem;'>Lahenduse ja easy arendust ning ülesannete loomist on toetanud:</p>
-                <img style='width: 20rem;' src='static/img/logo/ati.svg' alt='Tartu Ülikool arvutiteaduse instituut'>
+                
+                <ez-about-sponsors>
+                    <img style='width: 25rem; padding: 1rem;' src='static/img/logo/harno.svg' alt=''>
+                    <img style='width: 25rem; padding: 1rem;' src='static/img/logo/mkm.png' alt=''>
+                    <img style='width: 20rem; padding: 1rem;' src='static/img/logo/ita.png' alt=''>
+                </ez-about-sponsors>
                 
             </ez-about>
         """.trimIndent(),
