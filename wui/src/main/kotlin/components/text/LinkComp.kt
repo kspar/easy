d81e97640deef8a718478041be6b3c2d5dd0747f
kspar <kaspar@mnemonic.no>
2023-09-14 01:48:39 +0200

    wui link comp make the icon work

--- wui/src/main/kotlin/components/text/LinkComp.kt
+++ wui/src/main/kotlin/components/text/LinkComp.kt
@@ -5,24 +5,27 @@ import org.w3c.dom.HTMLAnchorElement
 import rip.kspar.ezspa.Component
 import rip.kspar.ezspa.IdGenerator
 import rip.kspar.ezspa.getElemByIdAs
-import tmRender
+import template
 
 
 class LinkComp(
-        private val label: String,
-        private val icon: String? = null,
-        private val loadingLabel: String? = null,
-        private val onClick: () -> Unit,
-        parent: Component?,
-        dstId: String = IdGenerator.nextId()
+    private val label: String,
+    private val icon: String? = null,
+    private val loadingLabel: String? = null,
+    private val onClick: () -> Unit,
+    parent: Component?,
+    dstId: String = IdGenerator.nextId()
 ) : Component(parent, dstId) {
 
     private val id: String = IdGenerator.nextId()
 
-    override fun render(): String = tmRender("t-c-link",
-            "id" to id,
-            "iconHtml" to icon,
-            "label" to label
+    override fun render(): String = template(
+        """
+            <ez-link><a id="{{id}}" href="#!">{{{iconHtml}}}{{label}}</a></ez-link>
+        """.trimIndent(),
+        "id" to id,
+        "iconHtml" to icon,
+        "label" to label
     )
 
     override fun postRender() {
