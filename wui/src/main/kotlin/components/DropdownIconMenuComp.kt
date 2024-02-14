1ea9a56cbd50b9c0e2ac85fa987586ecbd499267
kspar <kaspar@mnemonic.no>
2024-02-14 23:57:25 +0100

    wui dropdown icon menu now uses icon button internally

--- wui/src/main/kotlin/components/DropdownIconMenuComp.kt
+++ wui/src/main/kotlin/components/DropdownIconMenuComp.kt
@@ -2,41 +2,33 @@ package components
 
 import rip.kspar.ezspa.Component
 import rip.kspar.ezspa.IdGenerator
-import rip.kspar.ezspa.getElemById
-import rip.kspar.ezspa.onVanillaClick
 import template
 
-// TODO: might want to use IconButtonComp after it's been migrated
 class DropdownIconMenuComp(
     val icon: String,
+    val label: String?,
     val items: List<DropdownMenuComp.Item>,
     parent: Component
 ) : Component(parent) {
 
-    private val iconId = IdGenerator.nextId()
+    private val anchorId = IdGenerator.nextId()
 
-    private val dropdown = DropdownMenuComp(iconId, items, this)
+    private val iconBtn = IconButtonComp(
+        icon, label, onClick = { dropdown.toggleOpen() },
+        btnId = anchorId, parent = this
+    )
+
+    private val dropdown = DropdownMenuComp(anchorId, items, this)
 
     override val children: List<Component>
-        get() = listOf(dropdown)
-
+        get() = listOf(iconBtn, dropdown)
 
     override fun render() = template(
         """
             <ez-flex style='position: relative'>
-                <md-icon-button id="{{id}}">
-                    <md-icon>{{{icon}}}</md-icon>
-                </md-icon-button>
+                $iconBtn
                 $dropdown
             </ez-flex>
         """.trimIndent(),
-        "id" to iconId,
-        "icon" to icon,
     )
-
-    override fun postRender() {
-        getElemById(iconId).onVanillaClick(false) {
-            dropdown.toggleOpen()
-        }
-    }
 }
