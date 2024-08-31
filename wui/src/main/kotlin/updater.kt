411f0a8485021dbbc786d7d7defa8dd86bbbe325
kspar <kaspar@mnemonic.no>
2024-08-31 21:34:46 +0300

    EZ-1601 show toast again if dismissed by navigation, robot icon

--- wui/src/main/kotlin/updater.kt
+++ wui/src/main/kotlin/updater.kt
@@ -1,6 +1,8 @@
+
 import components.ToastThing
 import kotlinx.browser.window
 import kotlinx.coroutines.await
+import rip.kspar.ezspa.IdGenerator
 import rip.kspar.ezspa.doInPromise
 import rip.kspar.ezspa.getElemBySelector
 import rip.kspar.ezspa.sleep
@@ -8,6 +10,8 @@ import translation.Str
 
 
 private val currentVersion = getElemBySelector("head > meta[name=\"ez-wui-version\"]").getAttribute("content")
+
+private val toastId = IdGenerator.nextId()
 
 fun startPollingForStaticUpdate() = doInPromise {
     if (AppProperties.STATIC_UPDATE_ENABLED)
@@ -23,11 +27,11 @@ fun startPollingForStaticUpdate() = doInPromise {
                     ToastThing(
                         Str.lahendusUpdated,
                         ToastThing.Action(Str.doRefresh, { window.location.reload() }),
-                        icon = ToastThing.ERROR_INFO,
+                        icon = Icons.robot,
                         isDismissable = false,
-                        displayTime = ToastThing.PERMANENT
+                        displayTime = ToastThing.PERMANENT,
+                        id = toastId
                     )
-                    break
                 }
 
             } catch (e: Throwable) {
