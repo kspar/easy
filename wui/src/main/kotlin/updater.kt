d6fbee16ecaf28d3e63214c0103575d0a9ed268a
kspar <kaspar@mnemonic.no>
2024-08-31 14:55:35 +0300

    EZ-1601 allow disabling static update polling for local dev

--- wui/src/main/kotlin/updater.kt
+++ wui/src/main/kotlin/updater.kt
@@ -10,29 +10,30 @@ import translation.Str
 private val currentVersion = getElemBySelector("head > meta[name=\"ez-wui-version\"]").getAttribute("content")
 
 fun startPollingForStaticUpdate() = doInPromise {
-    while (true) {
-        try {
-            val latestVersion = window.fetch(
-                AppProperties.WUI_ROOT + "/static/version.txt",
-            ).await().text().await()
+    if (AppProperties.STATIC_UPDATE_ENABLED)
+        while (true) {
+            try {
+                val latestVersion = window.fetch(
+                    AppProperties.WUI_ROOT + "/static/version.txt",
+                ).await().text().await()
 
-            if (latestVersion != currentVersion) {
-                debug { "Static update found, current: $currentVersion, latest: $latestVersion" }
+                if (latestVersion != currentVersion) {
+                    debug { "Static update found, current: $currentVersion, latest: $latestVersion" }
 
-                ToastThing(
-                    Str.lahendusUpdated,
-                    ToastThing.Action(Str.doRefresh, { window.location.reload() }),
-                    icon = ToastThing.ERROR_INFO,
-                    isDismissable = false,
-                    displayTime = ToastThing.PERMANENT
-                )
-                break
+                    ToastThing(
+                        Str.lahendusUpdated,
+                        ToastThing.Action(Str.doRefresh, { window.location.reload() }),
+                        icon = ToastThing.ERROR_INFO,
+                        isDismissable = false,
+                        displayTime = ToastThing.PERMANENT
+                    )
+                    break
+                }
+
+            } catch (e: Throwable) {
+                warn { "Static update poll failed: ${e.message}" }
             }
 
-        } catch (e: Throwable) {
-            warn { "Static update poll failed: ${e.message}" }
+            sleep(AppProperties.STATIC_UPDATE_POLL_SEC * 1000).await()
         }
-
-        sleep(AppProperties.STATIC_UPDATE_POLL_SEC * 1000).await()
-    }
 }
