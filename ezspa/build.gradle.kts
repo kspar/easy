85d4d54c44a37e3b640fcaa8cdf0a07018890698
kspar <kaspar@mnemonic.no>
2024-11-28 23:20:31 +0200

    ezspa remove publishing tools/conf since they're old and cause problems when building from scratch

--- ezspa/build.gradle.kts
+++ ezspa/build.gradle.kts
@@ -5,8 +5,6 @@ val repoUrl = "https://github.com/kspar/easy/tree/master/ezspa"
 
 plugins {
     kotlin("js")
-    id("com.jfrog.bintray") version "1.8.5"
-    id("maven-publish")
 }
 
 group = mvnGroup
@@ -25,50 +23,5 @@ kotlin {
     js(IR) {
         browser {}
         binaries.executable()
-    }
-}
-
-bintray {
-    user = System.getProperty("bintray.user")
-    key = System.getProperty("bintray.key")
-    setPublications("BintrayPublication")
-    pkg.apply {
-        repo = mvnArtifact
-        name = mvnArtifact
-        version.name = mvnVersion
-        vcsUrl = repoUrl
-        setLicenses("MIT")
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("BintrayPublication") {
-            from(components["kotlin"])
-            groupId = mvnGroup
-            artifactId = mvnArtifact
-            version = mvnVersion
-
-            // Add sources too
-            artifact(tasks["kotlinSourcesJar"])
-
-            pom {
-                name.set(mvnArtifact)
-                description.set("Easy SPA microframework for Kotlin/JS")
-                url.set(repoUrl)
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("kspar")
-                        name.set("Kaspar Papli")
-                    }
-                }
-            }
-        }
     }
 }
