0d5a1eba2c593e4d5b8651929cadf56600de2374
kspar <kaspar@mnemonic.no>
2025-05-27 12:22:12 +0300

    EZ-1675 delete inactive users from keycloak

--- core/src/main/kotlin/core/ems/cron/delete_inactive_users.kt
+++ core/src/main/kotlin/core/ems/cron/delete_inactive_users.kt
@@ -1,32 +1,57 @@
 package core.ems.cron
 
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import core.db.*
 import mu.KotlinLogging
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.inList
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.*
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.RestTemplate
 import java.util.*
 
 
 @Component
 class DeleteInactiveUsers {
+    private val log = KotlinLogging.logger {}
+
     // Chosen by fair dice roll, guaranteed to be random - do not change
     private val defaultUser = "58060066-c4f9-4054-88be-c8492bb8e487"
 
-    private val log = KotlinLogging.logger {}
+    @Value("\${easy.core.keycloak.base-url}")
+    private lateinit var keycloakBaseUrl: String
+
+    @Value("\${easy.core.keycloak.realm}")
+    private lateinit var realm: String
+
+    @Value("\${easy.core.keycloak.client-id}")
+    private lateinit var clientId: String
+
+    @Value("\${easy.core.keycloak.client-secret}")
+    private lateinit var clientSecret: String
+
+    private data class TokenResponse(
+        @JsonProperty("access_token") val accessToken: String,
+    )
+
+    private data class KeycloakUser(
+        @JsonProperty("id") val id: String,
+    )
 
 
     // TODO: once a day, for now every 30 seconds for testing
     @Scheduled(cron = "*/30 * * * * *")
     fun cron() {
-        transaction {
+        val deletedAccounts = transaction {
             val twoYearsAgo = DateTime.now().minusYears(2)
             val fiveYearsAgo = DateTime.now().minusYears(5)
-
 
             Account.insertIgnoreAndGetId {
                 it[Account.id] = defaultUser
@@ -55,7 +80,7 @@ class DeleteInactiveUsers {
 
             if (accountsToDelete.isEmpty()) {
                 log.debug { "No inactive users qualifying for deletion found." }
-                return@transaction
+                return@transaction emptyList()
             }
 
             log.info { "Deleting inactive users: $accountsToDelete" }
@@ -121,9 +146,102 @@ class DeleteInactiveUsers {
             Account.deleteWhere {
                 Account.id inList accountsToDelete
             }
+
+            accountsToDelete
         }
-        // TODO: outside transaction, delete from Keycloak
-        //  DELETE /admin/realms/{realm}/users/{user-id}
-        //  https://www.keycloak.org/docs-api/latest/rest-api/index.html#_users
+
+        // Delete from Keycloak
+        val token = getAccessToken()
+
+        deletedAccounts.forEach {
+            val keycloakUserId = getKeycloakUserId(it, token)
+            log.info { "Deleting Keycloak user '$it' ($keycloakUserId)" }
+            if (keycloakUserId == null) {
+                log.error { "Cannot delete Keycloak user '$it'" }
+            } else {
+                //deleteKeycloakUser(keycloakUserId, token)
+            }
+        }
+        log.info { "Deleted Keycloak users" }
+    }
+
+    private fun getAccessToken(): String {
+        val tokenUrl = "$keycloakBaseUrl/auth/realms/master/protocol/openid-connect/token"
+
+        val headers = HttpHeaders().apply {
+            contentType = MediaType.APPLICATION_FORM_URLENCODED
+        }
+
+        val body = LinkedMultiValueMap<String, String>().apply {
+            add("grant_type", "client_credentials")
+            add("client_id", clientId)
+            add("client_secret", clientSecret)
+        }
+
+        val request = HttpEntity(body, headers)
+        val response = RestTemplate().postForObject(tokenUrl, request, TokenResponse::class.java)
+
+        return response?.accessToken ?: throw RuntimeException("Failed to get access token")
+    }
+
+    private fun getKeycloakUserId(username: String, accessToken: String): String? {
+        val searchUrl = "$keycloakBaseUrl/auth/admin/realms/$realm/users"
+
+        val headers = HttpHeaders().apply {
+            setBearerAuth(accessToken)
+        }
+
+        val uriVariables = mapOf(
+            "username" to username,
+            "exact" to "true"
+        )
+
+        val responseType = object : ParameterizedTypeReference<List<KeycloakUser>>() {}
+
+        val response = RestTemplate().exchange(
+            "$searchUrl?username={username}&exact={exact}",
+            HttpMethod.GET,
+            HttpEntity<Any>(headers),
+            responseType,
+            uriVariables
+        )
+
+        val body = response.body
+
+        if (body == null) {
+            log.error { "Keycloak user search returned null" }
+            return null
+        }
+
+        if (body.isEmpty()) {
+            log.error { "Keycloak user not found by username '$username'" }
+            return null
+        }
+
+        val id = body.singleOrNull()?.id
+
+        if (id == null) {
+            log.error { "Multiple Keycloak users found with username '$username': $body" }
+            return null
+        }
+
+        return id
+    }
+
+    private fun deleteKeycloakUser(userId: String, accessToken: String): Boolean {
+        val deleteUrl = "$keycloakBaseUrl/auth/admin/realms/$realm/users/$userId"
+
+        val headers = HttpHeaders().apply {
+            setBearerAuth(accessToken)
+        }
+
+        val response = RestTemplate().exchange(
+            deleteUrl,
+            HttpMethod.DELETE,
+            HttpEntity<Any>(headers),
+            Void::class.java
+        )
+
+        return response.statusCode == HttpStatus.NO_CONTENT
     }
 }
