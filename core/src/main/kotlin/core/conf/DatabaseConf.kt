f0163e50554eb00392f04aa3c066decf27952b1d
kspar <kaspar.papli@gmail.com>
2026-02-10 00:42:19 +0200

    core move db properties and liquibase conf to src/main/resources and run liquibase update on startup

--- core/src/main/kotlin/core/conf/DatabaseConf.kt
+++ core/src/main/kotlin/core/conf/DatabaseConf.kt
@@ -1,11 +1,13 @@
 package core.conf
 
+import liquibase.integration.spring.SpringLiquibase
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.DependsOn
 import javax.annotation.PostConstruct
 import javax.sql.DataSource
 
@@ -16,9 +18,18 @@ class DataSourceConf {
     fun dataSource(): DataSource {
         return DataSourceBuilder.create().build()
     }
+
+    @Bean
+    fun liquibase(dataSource: DataSource): SpringLiquibase {
+        return SpringLiquibase().apply {
+            this.dataSource = dataSource
+            changeLog = "classpath:/db/changelog.xml"
+        }
+    }
 }
 
 @Configuration
+@DependsOn("liquibase")
 class DatabaseInit(val dataSource: DataSource) {
     @PostConstruct
     fun init() {
